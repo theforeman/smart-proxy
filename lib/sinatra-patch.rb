3ee29b07c887d77cd428da3673c8060a1e38e80e
Ohad Levy <ohadlevy@gmail.com>
2011-01-25 17:20:12 +0200

    fixes #614 - proxy should support Daemon mode

--- lib/sinatra-patch.rb
+++ lib/sinatra-patch.rb
@@ -1,6 +1,7 @@
 require "sinatra/base"
 require "openssl"
 require "webrick/https"
+require "daemon"
 module Sinatra
   class Base
     # Run the Sinatra app as a self-hosted server using
@@ -10,6 +11,15 @@ module Sinatra
       handler      = detect_rack_handler
       handler_name = handler.name.gsub(/.*::/, '')
       puts "Starting Foreman Proxy on #{port} using #{handler_name}" unless handler_name =~/cgi/i
+
+      FileUtils.mkdir_p(File.join(APP_ROOT, 'tmp/pids'))
+
+      if SETTINGS.daemon
+        Process.daemon(true)
+        pid = "#{APP_ROOT}/tmp/pids/server.pid"
+        File.open(pid, 'w'){ |f| f.write(Process.pid) }
+        at_exit { File.delete(pid) if File.exist?(pid) }
+      end
       handler.run self, {:Host => bind, :Port => port}.merge(ssl_options) do |server|
         [:INT, :TERM].each { |sig| trap(sig) { quit!(server, handler_name) } }
         set :running, true
