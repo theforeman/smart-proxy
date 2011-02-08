aa7de29ac91240287dacfb6dd2716a5c3a12e096
Paul Kelly <paul.ian.kelly@googlemail.com>
2011-02-08 12:55:50 +0000

    Fixes #655 - add :json to log_halt
    Signed-off-by: Paul Kelly <paul.ian.kelly@googlemail.com>

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,6 +2,7 @@ class SmartProxy < Sinatra::Base
 
   helpers do
     def log_halt code, message
+      content_type :json if request.accept.include?("application/json")
       logger.error message
       halt code, message
     end
