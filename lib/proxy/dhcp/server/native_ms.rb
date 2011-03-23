685aae680a6569c37c650f41589188e69b1541c3
Paul kelly <paul.ian.kelly@googlemail.com>
2011-03-23 23:44:32 +0800

    Fixes 779 - windows 64 bit detection wrong
    Signed-off-by: Paul kelly <paul.ian.kelly@googlemail.com>

--- lib/proxy/dhcp/server/native_ms.rb
+++ lib/proxy/dhcp/server/native_ms.rb
@@ -108,7 +108,7 @@ module Proxy::DHCP
     def execute cmd, msg=nil, error_only=false
       tsecs = 5
       response = nil
-      interpreter = settings.x86_64 ? 'c:\windows\sysnative\cmd.exe' : 'c:\windows\system32\cmd.exe'
+      interpreter = SETTINGS.x86_64 ? 'c:\windows\sysnative\cmd.exe' : 'c:\windows\system32\cmd.exe'
       command  = interpreter + ' /c c:\Windows\System32\netsh.exe -c dhcp ' + "server #{name} #{cmd}"
 
       std_in = std_out = std_err = nil
