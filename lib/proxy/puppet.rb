5835453b39ea02f5f40748d1053b050c7ebf1c44
Corey Osman <corey@logicminds.biz>
2011-06-24 14:44:19 -0700

    Fix: 1006 -- puppetrun is depreciated in puppet 2.6
    Signed-off-by: Corey Osman <corey@logicminds.biz>

--- lib/proxy/puppet.rb
+++ lib/proxy/puppet.rb
@@ -4,14 +4,20 @@ module Proxy::Puppet
 
   class << self
     def run *hosts
-      puppetrun = which("puppetrun", ["/usr/sbin", "/usr/bin"])
+      # Search in /opt/ for puppet enterprise users
+      default_path = ["/usr/sbin", "/usr/bin", "/opt/puppet/bin"]
+      # search for puppet for users using puppet 2.6+
+      puppetrun = which("puppetrun", default_path) || which("puppet", default_path)
       sudo = which("sudo", "/usr/bin")
 
       unless puppetrun and sudo
         logger.warn "sudo or puppetrun binary was not found - aborting"
         return false
       end
-
+      # Append kick to the puppet command if we are not using the old puppetca command
+      if not puppetrun.include? 'puppetrun'
+        puppetrun << " kick"
+      end
       command = %x[#{sudo} #{puppetrun} --host #{hosts.join(" --host ")}]
       if command =~ /finished with exit code 0/
         return true
