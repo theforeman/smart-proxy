a60a8b914793dd6620533f473975fd48d931940e
Allan Feid <afeid@shutterstock.com>
2012-12-03 13:16:02 -0500

    fix up variable names and general ruby syntax

--- lib/proxy/dns/powerdns.rb
+++ lib/proxy/dns/powerdns.rb
@@ -14,25 +14,24 @@ module Proxy::DNS
     # create({ :fqdn => "node01.lab", :value => "3.100.168.192.in-addr.arpa",
     #          :type => "PTR"}
     def create
-      id = _get_domain_id
-      if ! id
-        raise(Proxy::DNS::Error, "Unable to determine zone. Zone must exist in PowerDNS.")
+      unless domain_id
+        raise Proxy::DNS::Error, "Unable to determine zone. Zone must exist in PowerDNS."
       end
 
       case @type
         when "A"
           if ip = dns_find(@fqdn)
-            raise(Proxy::DNS::Collision, "#{@fqdn} is already in use by #{ip}")
+            raise Proxy::DNS::Collision, "#{@fqdn} is already in use by #{ip}"
           else
             @mysql_connection.query("INSERT INTO records (records.domain_id,records.name,records.ttl,records.content,records.type) VALUES (#{id}, '#{@fqdn}', #{@ttl}, '#{@value}', '#{@type}');")
           end
         when "PTR"
-          _ptrname = @value.split(/\./).reverse * ".";
-          _ptrname.concat(".in-addr.arpa")
-          if name = dns_find(_ptrname)
-            raise(Proxy::DNS::Collision, "#{@value} is already used by #{name}")
+          ip = IPAddr.new(@value)
+          ptrname = ip.reverse
+          if name = dns_find(ptrname)
+            raise Proxy::DNS::Collision, "#{@value} is already used by #{name}"
           else
-            @mysql_connection.query("INSERT INTO records (records.domain_id,records.name,records.ttl,records.content,records.type) VALUES (#{id}, '#{_ptrname}', #{@ttl}, '#{@fqdn}', 'PTR');")
+            @mysql_connection.query("INSERT INTO records (records.domain_id,records.name,records.ttl,records.content,records.type) VALUES (#{id}, '#{ptrname}', #{@ttl}, '#{@fqdn}', 'PTR');")
           end
         end
     end
@@ -43,50 +42,50 @@ module Proxy::DNS
       when "A"
         @mysql_connection.query("DELETE FROM records WHERE name='#{@fqdn}'")
       when "PTR"
-        _ptrname = @value.split(/\./).reverse * ".";
-        _ptrname.concat(".in-addr.arpa")
-        @mysql_connection.query("DELETE FROM records WHERE name='#{_ptrname}'")
+        ip = IPAddr.new(@value)
+        ptrname = ip.reverse
+        @mysql_connection.query("DELETE FROM records WHERE name='#{ptrname}'")
       end
     end
 
     private
-    def _get_domain_id
+    def domain_id
       case @type
       when "A"
-        @_hostArray = @fqdn.split(/\./);
-        _search_depth = 1
+        host_list = @fqdn.split(/\./);
+        search_depth = 1
       when "PTR"
-        @_hostArray = @value.split(/\./).reverse;
-        _search_depth = 0
+        host_list = @value.split(/\./).reverse;
+        search_depth = 0
       end
 
-      _domain_id = nil
+      id = nil
 
-      while _domain_id == nil && @_hostArray.length != _search_depth && @_hostArray != nil
-        _domain = @_hostArray * "."
+      while id == nil && host_list.length != search_depth && host_list != nil
+        domain = host_list * "."
         if @type == "PTR"
-          _domain.concat(".in-addr.arpa")
+          domain.concat(".in-addr.arpa")
         end
-        _res = @mysql_connection.query("SELECT id FROM domains WHERE name = '#{_domain}' LIMIT 1;")
-        if _res.num_rows() != 0
-          _domain_id = _res.fetch_row
+        res = @mysql_connection.query("SELECT id FROM domains WHERE name = '#{domain}' LIMIT 1;")
+        if res.num_rows() != 0
+          id = res.fetch_row
         end
-        _res.free
-        @_hostArray.delete_at(0)
+        res.free
+        host_list.delete_at(0)
       end
-      _domain_id
+      id
     end
 
     private
-    def dns_find _key
-      _value = nil
-      _res = @mysql_connection.query("SELECT content FROM records WHERE name = '#{_key}' LIMIT 1;")
-      if _res.num_rows() != 0
-        _value = _res.fetch_row
+    def dns_find key
+      value = nil
+      res = @mysql_connection.query("SELECT content FROM records WHERE name = '#{key}' LIMIT 1;")
+      if res.num_rows() != 0
+        value = res.fetch_row
       end
-      _res.free
-      if _value != nil
-        _value
+      res.free
+      if value != nil
+        value
       else
         false
       end
