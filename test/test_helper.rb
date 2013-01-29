4884d4ac55993b23c244307572728dc5fe502998
Jan Vansteenkiste <jan@vstone.eu>
2013-01-29 11:38:06 +0100

    Fixes some tests that could not find Proxy::PuppetCA and Proxy::TFTP

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 require "test/unit"
 $: << File.join(File.dirname(__FILE__), '..', 'lib')
 require "proxy"
+require "proxy/puppetca"
+require "proxy/tftp"
 require "mocha/setup"
 require "rack/test"
 require 'sinatra'
