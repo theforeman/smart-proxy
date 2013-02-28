0ca84d779a68208746fa885689e333b09da652d6
Mike Doherty <doherty@cs.dal.ca>
2013-02-27 19:55:14 -0800

    Make rm an HTTP DELETE

--- lib/squid_api.rb
+++ lib/squid_api.rb
@@ -1,18 +1,24 @@
 # Should only be accessible by Foreman
 class SmartProxy
-  # Having two handlers isn't DRY
-  post %r{/squid/(add|rm)$} do
-    require 'pp'
-    pp params
-
-    action = params[:captures].first
+  post '/squid/add' do
     host   = params[:host]
 
     begin
-      log_halt 400, "Couldn't #{action} squid ACL: no host specified" unless host
-      log_halt 500, "Failed to #{action} squid ACL" unless Proxy::Squid.send action.to_sym, host
+      log_halt 400, "Couldn't add squid ACL: no host specified" unless host
+      log_halt 500, "Failed to add squid ACL" unless Proxy::Squid.add host
     rescue => e
-      log_halt 500, "Failed to #{action} squid configuration: #{e}"
+      log_halt 500, "Failed to add squid configuration: #{e}"
+    end
+  end
+
+  delete '/squid/rm' do
+    host   = params[:host]
+
+    begin
+      log_halt 400, "Couldn't remove squid ACL: no host specified" unless host
+      log_halt 500, "Failed to remove squid ACL" unless Proxy::Squid.rm host
+    rescue => e
+      log_halt 500, "Failed to remove squid configuration: #{e}"
     end
   end
 
