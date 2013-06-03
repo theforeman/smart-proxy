c4a245d514b1ce6de6c3c79a3e458e52caa4dc7e
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2013-06-03 18:44:52 +0100

    fixes #2520 - defines default settings for dns and puppet

--- lib/proxy/settings.rb
+++ lib/proxy/settings.rb
@@ -2,18 +2,30 @@ require "checks"
 require "yaml"
 require "ostruct"
 require "pathname"
-raw_config = File.read(Pathname.new(__FILE__).join("..", "..","..","config","settings.yml"))
+
+require 'proxy/dns/default_dns_settings'
+require 'proxy/puppet/default_puppet_settings'
 
 class Settings < OpenStruct
-  def method_missing args
+  DEFAULTS = [Proxy::DNS::DefaultSettings::DEFAULTS, Proxy::Puppet::DefaultSettings::DEFAULTS].inject({}) do |all, current|
+    all.merge!(current)
+  end
+
+  def self.load_from_file(settings_path = Pathname.new(__FILE__).join("..", "..","..","config","settings.yml"))
+    settings = YAML.load(File.read(settings_path))
+    if PLATFORM =~ /mingw/
+      settings.delete :puppetca if settings.has_key? :puppetca
+      settings.delete :puppet   if settings.has_key? :puppet
+      settings[:x86_64] = File.exist?('c:\windows\sysnative\cmd.exe')
+    end
+    load(settings)
+  end
+
+  def self.load(ahash)
+    Settings.new(DEFAULTS.merge(ahash))
+  end
+
+  def method_missing(symbol, *args)
     false
   end
 end
-
-settings = YAML.load(raw_config)
-if PLATFORM =~ /mingw/
-  settings.delete :puppetca if settings.has_key? :puppetca
-  settings.delete :puppet   if settings.has_key? :puppet
-  settings[:x86_64] = File.exist?('c:\windows\sysnative\cmd.exe')
-end
-SETTINGS = Settings.new(settings)
--- test/tftp_test.rb
+++ test/tftp_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class TftpTest < Test::Unit::TestCase
-
   def setup
     @tftp = Proxy::TFTP::Tftp.new
   end
@@ -10,8 +9,17 @@ class TftpTest < Test::Unit::TestCase
     assert_respond_to @tftp, :logger
   end
 
-  def test_should_create_tftp_link
-    assert_equal @tftp.send(:path), SETTINGS.tftproot ? SETTINGS.tftproot : File.dirname(__FILE__) + "../tftpboot"
+  def test_path_to_tftp_directory_without_tftproot_setting
+    assert_equal Pathname.new(__FILE__).join("..", "..", "lib","proxy","tftpboot").to_s, @tftp.send(:path)
   end
 
+  def test_path_to_tftp_directory_with_tftproot_setting
+    SETTINGS.stubs(:tftproot).returns("/some/tftp/root")
+    assert_equal SETTINGS.tftproot, @tftp.send(:path)
+  end
+
+  def test_path_to_tftp_directory_with_relative_tftproot_setting
+    SETTINGS.stubs(:tftproot).returns("./some/root")
+    assert_equal Pathname.new(__FILE__).join("..", "..", "lib","proxy","some","root").to_s, @tftp.send(:path)
+  end
 end
