5c7223f464e80b635d5926c85b1cb1d808b04da1
Daniel Baeurer <daniel.baeurer@gmail.com>
2013-07-03 10:04:24 +0200

    Fix tests for multiple dynamic module paths

--- test/puppet_environment_test.rb
+++ test/puppet_environment_test.rb
@@ -92,7 +92,7 @@ class PuppetEnvironmentTest < Test::Unit::TestCase
     env = Proxy::Puppet::Environment.all
     assert_array_equal env.map { |e| e.name }, ['dev','prod']
     env_class_map = env.map { |e| [e.name, e.classes.map { |c| c.name }].flatten }
-    assert_array_equal env_class_map, [["prod", "test1"], ["prod", "test2"], ["dev", "test3"], ["dev", "test4"]]
+    assert_array_equal env_class_map, [["prod", "test1", "test2"], ["dev", "test3", "test4"]]
   end
 
   def test_multiple_modulepath_in_single_env_with_multiple_dynamic_path_and_static_path
@@ -102,9 +102,9 @@ class PuppetEnvironmentTest < Test::Unit::TestCase
     }
     Puppet.settings.stubs(:instance_variable_get).returns(config)
     env = Proxy::Puppet::Environment.all
-    assert_array_equal env.map { |e| e.name }, ['dev','prod']
+    assert_array_equal env.map { |e| e.name }, ['master','dev','prod']
     env_class_map = env.map { |e| [e.name, e.classes.map { |c| c.name }].flatten }
-    assert_array_equal env_class_map, [["prod", "test"], ["prod", "test1"], ["prod", "test2"], ["dev", "test3"], ["dev", "test4"]]
+    assert_array_equal env_class_map, [["master", "test"], ["prod", "test1", "test2"], ["dev", "test3", "test4"]]
   end
 
   def test_multiple_modulepath_in_single_env_with_dynamic_path
