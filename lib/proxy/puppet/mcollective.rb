a8567914ebb450e126cdb088179b08720ab30522
Greg Petras <greg.petras@nwea.org>
2013-10-03 20:22:12 +0000

    fixed feature #3150 - adds ability to specify the user to invoke sudo as when calling mco

--- lib/proxy/puppet/mcollective.rb
+++ lib/proxy/puppet/mcollective.rb
@@ -5,6 +5,11 @@ module Proxy::Puppet
     def run
       cmd = []
       cmd.push(which("sudo", "/usr/bin"))
+
+      if SETTINGS.puppet_mco_user
+        cmd.push("-u",SETTINGS.puppet_mco_user)
+      end
+
       cmd.push(which("mco", ["/usr/bin", "/opt/puppet/bin"]))
 
       if cmd.include?(false)
