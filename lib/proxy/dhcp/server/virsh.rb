2117040d4e1313fdf50b171b604e03f10af963f5
Dominic Cleal <dcleal@redhat.com>
2014-03-14 13:06:10 +0000

    fixes #4661 - store hostname with virsh DHCP reservations

--- lib/proxy/dhcp/server/virsh.rb
+++ lib/proxy/dhcp/server/virsh.rb
@@ -38,7 +38,8 @@ module Proxy::DHCP
         REXML::XPath.each(doc, "//network/ip[not(@family) or @family='ipv4']/dhcp/host") do |e|
           Proxy::DHCP::Record.new(:subnet => subnet,
                                   :ip => e.attributes["ip"],
-                                  :mac => e.attributes["mac"])
+                                  :mac => e.attributes["mac"],
+                                  :hostname => e.attributes["name"])
         end
       rescue Exception => e
         msg = "DHCP virsh provider error: unable to retrive virsh info: #{e}"
@@ -50,13 +51,13 @@ module Proxy::DHCP
 
     def addRecord options={}
       record = super(options)
-      virsh_update_dhcp 'add-last', record.mac, record.ip
+      virsh_update_dhcp 'add-last', record.mac, record.ip, record.name
       record
     end
 
     def delRecord subnet, record
       super(subnet, record)
-      virsh_update_dhcp 'delete', record.mac, record.ip
+      virsh_update_dhcp 'delete', record.mac, record.ip, record[:hostname]
     end
   end
 end
