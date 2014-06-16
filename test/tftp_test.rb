4594dbb3dc616ea14499c63aad3507a74fa46685
Greg Sutcliffe <gsutclif@redhat.com>
2014-06-16 12:54:30 +0100

    Fixes #6086 - stop remote command execution and path exploit in TFTP API (CVE-2014-0007)
    (cherry picked from commit 854ab5573df152fd20b2be5547a08b4862fb78fe)

--- test/tftp_test.rb
+++ test/tftp_test.rb
@@ -22,4 +22,21 @@ class TftpTest < Test::Unit::TestCase
     SETTINGS.stubs(:tftproot).returns("./some/root")
     assert_equal Pathname.new(__FILE__).join("..", "..", "lib","proxy","some","root").to_s, @tftp.send(:path)
   end
+
+  def test_paths_inside_tftp_directory_dont_raise_errors
+    SETTINGS.stubs(:tftproot).returns("/some/root")
+    Proxy::Util::CommandTask.stubs(:new).returns(true)
+    FileUtils.stubs(:mkdir_p).returns(true)
+    assert Proxy::TFTP.send(:fetch_boot_file,'/some/root/boot/file','http://localhost/file')
+  end
+
+  def test_paths_outside_tftp_directory_raise_errors
+    SETTINGS.stubs(:tftproot).returns("/some/root")
+    Proxy::Util::CommandTask.stubs(:new).returns(true)
+    FileUtils.stubs(:mkdir_p).returns(true)
+    assert_raises RuntimeError do
+      Proxy::TFTP.send(:fetch_boot_file,'/other/root/boot/file','http://localhost/file')
+    end
+  end
+
 end
