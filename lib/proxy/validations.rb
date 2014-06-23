5f46b1bcaf6a7a802e9abbf83e7baf9168016e7d
Trey Dockendorf <treydock@gmail.com>
2014-06-23 11:07:55 -0500

    fixes #6341 - Add support for 64-bit MAC addresses

--- lib/proxy/validations.rb
+++ lib/proxy/validations.rb
@@ -1,18 +1,27 @@
 module Proxy::Validations
 
+  MAC_REGEXP_48BIT = /\A([a-f0-9]{1,2}:){5}[a-f0-9]{1,2}\z/i
+  MAC_REGEXP_64BIT = /\A([a-f0-9]{1,2}:){19}[a-f0-9]{1,2}\z/i
+
   class Error < RuntimeError; end
   private
+
+  def valid_mac? mac
+    return false if mac.nil?
+    return true if mac =~ MAC_REGEXP_48BIT or mac =~ MAC_REGEXP_64BIT
+    false
+  end
 
   # validates the ip address
   def validate_ip ip
     raise Error, "Invalid IP Address #{ip}" unless ip =~ /(\d{1,3}\.){3}\d{1,3}/
-      ip
+    ip
   end
 
   # validates the mac
   def validate_mac mac
-    raise Error, "Invalid MAC #{mac}" unless mac =~ /([a-f0-9]{1,2}:){5}[a-f0-9]{1,2}/i
-      mac.downcase
+    raise Error, "Invalid MAC #{mac}" unless valid_mac?(mac)
+    mac.downcase
   end
 
   def validate_subnet subnet
