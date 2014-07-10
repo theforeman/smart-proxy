96e05173750a47e81e308393f014703c6a59d512
Aaron Stone <aaron@serendipity.cx>
2014-07-10 03:17:54 -0700

    Fixes #6742 - Add a unit test suite for the DHCP API
    This is built around the ISC DHCPD provider, the goal is testing the API layer.

---
+++ test/dhcp/dhcp_api_test.rb
@@ -0,0 +1,189 @@
+require 'test_helper'
+require 'json'
+require 'ostruct'
+require 'sinatra'
+require 'dhcp/dhcp/dhcp_api'
+
+ENV['RACK_ENV'] = 'test'
+
+class DhcpApiTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  # Run tests in alphabetical order for this file
+  def self.test_order
+    :sorted # default is random order
+  end
+
+  def app
+    Proxy::DhcpApi.new
+  end
+
+  def setup
+    Proxy::DhcpPlugin.load_test_settings(
+      :enabled => true,
+      :dhcp_vendor => 'isc',
+      :dhcp_config => './test/fixtures/dhcp/dhcp.conf',
+      :dhcp_leases => './test/fixtures/dhcp/dhcp.leases',
+      :dhcp_subnets => '192.168.122.0/255.255.255.0')
+
+    Proxy::DHCP::Server::ISC.any_instance.stubs(:omcmd)
+  end
+
+  def test_api_01_get_root
+    get "/"
+    assert last_response.ok?, "Last response was not ok: #{last_response.status} #{last_response.body}"
+    data = JSON.parse(last_response.body)
+    expected = [{"network"=>"192.168.122.0", "netmask"=>"255.255.255.0"}]
+    assert_equal expected, data
+  end
+
+  def test_api_02_get_network
+    get "/192.168.122.0"
+    assert last_response.ok?, "Last response was not ok: #{last_response.status} #{last_response.body}"
+    data = JSON.parse(last_response.body)
+    expected = {
+      "reservations" => [ {
+        "hostname"   => "test.example.com",
+        "ip"         => "192.168.122.1",
+        "mac"        => "00:11:bb:cc:dd:ee",
+      } ],
+      "leases"       => [ {
+        "ip"         => "192.168.122.2",
+        "mac"        => "00:aa:bb:cc:dd:ee",
+        "starts"     => RUBY_VERSION =~ /^1\.8/ ? "Sat Jul 12 10:08:29 UTC 2014" : "2014-07-12 10:08:29 UTC",
+        "ends"       => nil,
+        "state"      => "active",
+      } ]
+    }
+    assert_equal expected, data
+  end
+
+  def test_api_03_get_network_unused_ip
+    get "/192.168.122.0/unused_ip"
+    assert last_response.ok?, "Last response was not ok: #{last_response.status} #{last_response.body}"
+    data = JSON.parse(last_response.body)
+    expected = {"ip"=>"192.168.122.3"}
+    assert_equal expected, data
+
+    assert_equal true, File.exists?('test/tmp/foreman-proxy_192.168.122.0_24.tmp')
+    assert_equal '1', File.read('test/tmp/foreman-proxy_192.168.122.0_24.tmp')
+  end
+
+  def test_api_04_get_network_unused_ip_again
+    get "/192.168.122.0/unused_ip"
+    assert last_response.ok?, "Last response was not ok: #{last_response.status} #{last_response.body}"
+    data = JSON.parse(last_response.body)
+    expected = {"ip"=>"192.168.122.4"}
+    assert_equal expected, data
+
+    assert_equal true, File.exists?('test/tmp/foreman-proxy_192.168.122.0_24.tmp')
+    assert_equal '2', File.read('test/tmp/foreman-proxy_192.168.122.0_24.tmp')
+  end
+
+  def test_api_05_get_network_record
+    get "/192.168.122.0/192.168.122.1"
+    assert last_response.ok?, "Last response was not ok: #{last_response.status} #{last_response.body}"
+    data = JSON.parse(last_response.body)
+    expected = {
+      "hostname" =>"test.example.com",
+      "ip"       =>"192.168.122.1",
+      "mac"      =>"00:11:bb:cc:dd:ee",
+      "subnet"   =>"192.168.122.0/255.255.255.0"
+    }
+    assert_equal expected, data
+  end
+
+  # New record with identical duplicate contents should be a successful no-op
+  def test_api_06_create_record_existing
+    params = {
+      "hostname" => "test.example.com",
+      "ip"       => "192.168.122.1",
+      "mac"      => "00:11:bb:cc:dd:ee",
+      "network"  => "192.168.122.0/255.255.255.0",
+    }
+    post "/192.168.122.0", params
+    assert last_response.ok?, "Last response was not ok: #{last_response.status} #{last_response.body}"
+    expected = ''
+    assert_equal expected, last_response.body
+  end
+
+  # New record with conflicting contents should be a failure
+  def test_api_07_create_record_collision
+    params = {
+      "hostname" => "test.example.com",
+      "ip"       => "192.168.122.1",
+      "mac"      => "00:aa:bb:cc:dd:ee",
+      "network"  => "192.168.122.0/255.255.255.0",
+    }
+    post "/192.168.122.0", params
+    assert last_response.client_error?, "Last response was not ok: #{last_response.status} #{last_response.body}"
+    expected = 'Record 192.168.122.0/192.168.122.1 already exists'
+    assert_equal expected, last_response.body
+    assert_equal 409, last_response.status
+  end
+
+  def test_api_08_create_record_new
+    params = {
+      "hostname" => "ten.example.com",
+      "ip"       => "192.168.122.10",
+      "mac"      => "10:10:10:10:10:10",
+      "network"  => "192.168.122.0/255.255.255.0",
+      # TODO: test some optional vendor options here
+    }
+    post "/192.168.122.0", params
+    assert last_response.ok?, "Last response was not ok: #{last_response.status} #{last_response.body}"
+    expected = ''
+    assert_equal expected, last_response.body
+
+    # We stubbed out the real omcmd, so fake it:
+    File.open("test/tmp/dhcp_extra_leases.tmp", File::RDWR|File::CREAT) do |f|
+      f.puts <<-EOF
+        host ten.example.com {
+          dynamic;
+          hardware ethernet 10:10:10:10:10:10;
+          fixed-address 192.168.122.10;
+          supersede host-name = "ten.example.com";
+        }
+      EOF
+    end
+
+    # Repeat the load subnet test, make sure the new entry is in there
+    get "/192.168.122.0"
+    assert last_response.ok?, "Last response was not ok: #{last_response.status} #{last_response.body}"
+    data = JSON.parse(last_response.body)
+    expected = {
+      "reservations" => [ {
+        "hostname"   => "test.example.com",
+        "ip"         => "192.168.122.1",
+        "mac"        => "00:11:bb:cc:dd:ee",
+      }, {
+        "hostname"   => "ten.example.com",
+        "ip"         => "192.168.122.10",
+        "mac"        => "10:10:10:10:10:10",
+      } ],
+      "leases"       => [ {
+        "ip"         => "192.168.122.2",
+        "mac"        => "00:aa:bb:cc:dd:ee",
+        "starts"     => RUBY_VERSION =~ /^1\.8/ ? "Sat Jul 12 10:08:29 UTC 2014" : "2014-07-12 10:08:29 UTC",
+        "ends"       => nil,
+        "state"      => "active",
+      } ]
+    }
+    assert_equal expected, data
+  end
+
+  def test_api_09_delete_record
+    delete "/192.168.122.0/192.168.122.10"
+    assert last_response.ok?, "Last response was not ok: #{last_response.status} #{last_response.body}"
+    expected = ''
+    assert_equal expected, last_response.body
+  end
+
+  def test_api_10_delete_record_notfound
+    delete "/192.168.122.0/192.168.122.11"
+    assert last_response.not_found?, "Last response was not ok: #{last_response.status} #{last_response.body}"
+    expected = 'Record 192.168.122.0/192.168.122.11 not found'
+    assert_equal expected, last_response.body
+  end
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,11 @@ $: << File.join(File.dirname(__FILE__), '..', 'modules')
 logdir = File.join(File.dirname(__FILE__), '..', 'logs')
 FileUtils.mkdir_p(logdir) unless File.exists?(logdir)
 
+# Make sure that tests put their temp files in a controlled location
+# Clear temp file before each test run
+ENV['TMPDIR'] = 'test/tmp'
+FileUtils.rm_f Dir.glob 'test/tmp/*.tmp'
+
 require "mocha/setup"
 require "rack/test"
 
