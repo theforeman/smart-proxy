2cba6063e429fa6aed365e8950200207c2fb8a5c
Aaron Stone <aaron@serendipity.cx>
2014-07-11 12:37:56 -0700

    fixes #6700 - tests for Proxy::Puppet::Runner shell methods

--- test/puppet/runner_test.rb
+++ test/puppet/runner_test.rb
@@ -3,11 +3,20 @@ require 'puppet_proxy/runner'
 
 class RunnerTest < Test::Unit::TestCase
   def setup
-    @runner = Proxy::Puppet::Runner.new({})
+    @runner = Proxy::Puppet::Runner.new(:nodes => ['foo', 'bar', 'foo bar'])
   end
 
-  def test_popen
-    @runner.send(:popen, ["echo", "blah"])
-    assert_equal 0, $?.exitstatus
+  def test_shell_escaped_nodes
+    assert_equal ['foo', 'bar', 'foo\ bar'], @runner.send(:shell_escaped_nodes)
+  end
+
+  def test_shell_command_true
+    success = @runner.send(:shell_command, ['true'])
+    assert_equal true, success
+  end
+
+  def test_shell_command_false
+    success = @runner.send(:shell_command, ['false'])
+    assert_equal false, success
   end
 end
