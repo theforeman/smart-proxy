63a9e50ad66f79fb2319f9ad9c4feb3693543e2b
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2014-08-22 16:44:39 +0100

    Fixes #7438: moved facter loading into the facter plugin descriptor

--- modules/facts/facts.rb
+++ modules/facts/facts.rb
@@ -1,8 +1,1 @@
-include ::Proxy::Log
-begin
-  require "facter"
-rescue LoadError
-  logger.info "Facter was not found, Facts API disabled"
-end
-
-require 'facts/facts_plugin' if defined?(:Facter)
+require 'facts/facts_plugin'
