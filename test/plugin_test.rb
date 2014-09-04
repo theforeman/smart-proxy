9824d1c588c52d50249d48a98993e72b543f33b7
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2014-09-04 11:51:00 +0100

    fixes #7351: plugins no longer crash smart-proxy if they are missing either of rackup paths

--- test/plugin_test.rb
+++ test/plugin_test.rb
@@ -14,4 +14,10 @@ class PluginTest < Test::Unit::TestCase
 
     assert_equal ':a: a, :b: b, :enabled: false', plugin.log_used_default_settings
   end
+
+  def test_http_rackup_returns_empty_string_with_missing_rackup_path
+    eval "class TestPlugin2 < Proxy::Plugin; end"
+    assert_equal "", TestPlugin2.new.http_rackup
+    assert_equal "", TestPlugin2.new.https_rackup
+  end
 end
