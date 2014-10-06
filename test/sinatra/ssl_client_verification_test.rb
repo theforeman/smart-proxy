a49993bd0722d3a970fee2329b53c22e5e9c2e46
Dominic Cleal <dcleal@redhat.com>
2014-10-06 14:36:27 +0100

    fixes #7822 - forbid HTTPS requests with no client SSL certificate

---
+++ test/sinatra/ssl_client_verification_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+require 'json'
+require 'sinatra/base'
+
+ENV['RACK_ENV'] = 'test'
+
+class SSLClientVerificationTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    TestApp.new
+  end
+
+  def test_http
+    get '/test'
+    assert last_response.ok?
+  end
+
+  ['yes', 'on', '1'].each do |yes|
+    define_method("test_https_no_cert_https_#{yes}") do
+      get '/test', {}, {'HTTPS' => yes}
+      assert last_response.forbidden?
+    end
+  end
+
+  def test_https_cert
+    get '/test', {}, {'HTTPS' => 'on', 'SSL_CLIENT_CERT' => '...'}
+    assert last_response.ok?
+  end
+
+  private
+
+  class TestApp < ::Sinatra::Base
+    get '/test' do
+      'success'
+    end
+  end
+end
