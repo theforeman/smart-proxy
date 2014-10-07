d4af25cf39c4a9b0f5ac074342845fc439365ea1
Dominic Cleal <dominic@cleal.org>
2014-10-07 10:36:43 +0100

    fixes #7832 - add SSL verification tests for production Rack apps
    Uses the Rack app configurations built by the smart proxy launcher with
    the Sinatra authz helpers inside a WEBrick server running as part of the
    integration test. Real requests are made against the server to test the
    SSL configuration and helper behaviours.

---
+++ test/sinatra/ssl_client_verification_integration_test.rb
@@ -0,0 +1,78 @@
+require 'test_helper'
+require 'net/http'
+
+class SSLClientVerificationIntegrationTest < Test::Unit::TestCase
+  include Proxy::IntegrationTestCase
+
+  class TestAPIWithSSLClientAuth < ::Sinatra::Base
+    helpers ::Proxy::Helpers
+    authorize_with_ssl_client
+    get('/') { 'Success' }
+  end
+
+  class TestPluginWithSSLClientAuth < ::Proxy::Plugin
+    class << self
+      def http_rackup
+        'run SSLClientVerificationIntegrationTest::TestAPIWithSSLClientAuth'
+      end
+      alias https_rackup http_rackup
+    end
+  end
+
+  def test_http
+    launch protocol: 'http', plugins: [TestPluginWithSSLClientAuth]
+    res = Net::HTTP.get_response('localhost', '/', @settings.http_port)
+    assert_kind_of Net::HTTPSuccess, res
+    assert_equal 'Success', res.body
+  end
+
+  def test_https_no_cert
+    launch_https
+    http = Net::HTTP.new('localhost', @settings.https_port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    res = http.get('/')
+    assert_kind_of Net::HTTPForbidden, res
+  end
+
+  def test_https_cert_from_different_authority
+    launch_https
+    http = Net::HTTP.new('localhost', @settings.https_port)
+    http.use_ssl = true
+    http.ca_file = File.join(fixtures, 'certs', 'ca.pem')
+    http.cert    = OpenSSL::X509::Certificate.new(File.read(File.join(fixtures, 'certs', 'badclient.example.com.pem')))
+    http.key     = OpenSSL::PKey::RSA.new(File.read(File.join(fixtures, 'private_keys', 'badclient.example.com.pem')))
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    assert_raise OpenSSL::SSL::SSLError do
+      http.get('/')
+    end
+  end
+
+  def test_https_cert
+    launch_https
+    http = Net::HTTP.new('localhost', @settings.https_port)
+    http.use_ssl = true
+    http.ca_file = File.join(fixtures, 'certs', 'ca.pem')
+    http.cert    = OpenSSL::X509::Certificate.new(File.read(File.join(fixtures, 'certs', 'client.example.com.pem')))
+    http.key     = OpenSSL::PKey::RSA.new(File.read(File.join(fixtures, 'private_keys', 'client.example.com.pem')))
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    res = http.get('/')
+    assert_kind_of Net::HTTPSuccess, res
+    assert_equal 'Success', res.body
+  end
+
+  private
+
+  def launch_https
+    launch protocol: 'https', plugins: [TestPluginWithSSLClientAuth],
+           settings: {
+             ssl_private_key: File.join(fixtures, 'private_keys', 'server.example.com.pem'),
+             ssl_certificate: File.join(fixtures, 'certs', 'server.example.com.pem'),
+             ssl_ca_file:     File.join(fixtures, 'certs', 'ca.pem')
+           }
+  end
+
+  def fixtures
+    File.expand_path(File.join(File.dirname(__FILE__), '..', 'fixtures', 'ssl'))
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,8 @@ $: << File.join(File.dirname(__FILE__), '..', 'modules')
 logdir = File.join(File.dirname(__FILE__), '..', 'logs')
 FileUtils.mkdir_p(logdir) unless File.exist?(logdir)
 
+ENV['RACK_ENV'] = 'test'
+
 # Make sure that tests put their temp files in a controlled location
 # Clear temp file before each test run
 ENV['TMPDIR'] = 'test/tmp'
@@ -14,6 +16,8 @@ FileUtils.rm_f Dir.glob 'test/tmp/*.tmp'
 
 require "mocha/setup"
 require "rack/test"
+require 'timeout'
+require 'webmock/test_unit'
 
 require 'smart_proxy_for_testing'
 require 'provider_interface_validation/dhcp_provider'
@@ -22,4 +26,42 @@ include DhcpProviderInterfaceValidation
 
 def hash_symbols_to_strings(hash)
   Hash[hash.collect{|k,v| [k.to_s,v]}]
+end
+
+# Starts up a real smart proxy instance under WEBrick
+# Use sparingly.  API tests should use rack-test etc.
+module Proxy::IntegrationTestCase
+  include Proxy::Log
+
+  def setup
+    WebMock.allow_net_connect!
+  end
+
+  def launch(protocol: 'https', plugins: [], settings: {})
+    port = 1024 + rand(63_000)
+    @settings = Proxy::Settings::Global.new(settings.merge("#{protocol}_port" => port))
+    @t = Thread.new do
+      launcher = Proxy::Launcher.new(@settings)
+      app = launcher.public_send("#{protocol}_app", port, plugins)
+      launcher.webrick_server(app.merge(AccessLog: [Logger.new('/dev/null')]), ['localhost'], port).start
+    end
+    Timeout::timeout(2) do
+      sleep(0.1) until can_connect?('localhost', @settings.send("#{protocol}_port"))
+    end
+  end
+
+  def can_connect?(host, port)
+    TCPSocket.new(host, port).close
+    true
+  rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+    false
+  end
+
+  def teardown
+    WebMock.disable_net_connect!
+    if @t
+      Thread.kill(@t)
+      @t.join(30)
+    end
+  end
 end
