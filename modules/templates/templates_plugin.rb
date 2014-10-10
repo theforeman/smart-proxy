ca612f53bc8902584fc052dd90599b6a3cb19847
Dustin Tsang <dtsang@redhat.com>
2014-10-10 00:20:46 -0400

    Refs #969 - Proxy-side changes for serving templates from the proxy
    An update to @GregSutcliffe's original PR. Ports his original feature to the new
    plugin api.

---
+++ modules/templates/templates_plugin.rb
@@ -0,0 +1,21 @@
+module Proxy::Templates
+  class Plugin < ::Proxy::Plugin
+    http_rackup_path File.expand_path("http_config.ru", File.expand_path("../", __FILE__))
+    https_rackup_path File.expand_path("http_config.ru", File.expand_path("../", __FILE__))
+
+    default_settings :enabled => true
+    plugin :templates, ::Proxy::VERSION
+
+    after_activation do
+      unless Proxy::SETTINGS.foreman_url
+        logger.warn "missing :foreman_url: from configurations. 'templates' module is disabled"
+        ::Proxy::Plugins.disable_plugin(:templates)
+      end
+
+      unless Proxy::Templates::Plugin.settings.template_url
+        logger.warn "missing :templates_url: from configurations. 'templates' module is disabled"
+        ::Proxy::Plugins.disable_plugin(:templates)
+      end
+    end
+  end
+end
