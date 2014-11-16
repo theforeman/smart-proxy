f53285ed75383fe631176156f346622f49d33f53
Ori Rabin <orabin@redhat.com>
2014-11-16 15:41:21 +0200

    Fixes #8347 - facts feature should not be enabled by default

--- modules/facts/facts_plugin.rb
+++ modules/facts/facts_plugin.rb
@@ -2,7 +2,7 @@ class Proxy::FactsPlugin < ::Proxy::Plugin
   http_rackup_path File.expand_path("http_config.ru", File.expand_path("../", __FILE__))
   https_rackup_path File.expand_path("http_config.ru", File.expand_path("../", __FILE__))
 
-  default_settings :enabled => true
+  default_settings :enabled => false
   plugin :facts, ::Proxy::VERSION
 
   after_activation do
