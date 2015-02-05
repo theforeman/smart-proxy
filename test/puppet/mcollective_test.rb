b48239a910ee6ca2096274592113a337dc77859e
Corey Osman <corey@logicminds.biz>
2015-02-05 12:45:33 -0800

    Fixes #9245 - Add override setting to allow mcollective puppet runs to run as a different user
       * previously we could set the mcollective user via puppet_user setting. However, this could be misleading
         to the user as its not obvious where it was used in the smart proxy.  With the new mcollective user
         setting its pretty clear what the intention of the user is for.  In setups like Puppet Enterprise
         there is usually a user for mco and puppet which we have to keep separate and thus require a different user.
    
       * This adds a new setting to the puppet config called :mcollective_user:
    
    (cherry picked from commit 75f587114cee27f084839f7e0d2cde013b986b7f)

--- test/puppet/mcollective_test.rb
+++ test/puppet/mcollective_test.rb
@@ -25,6 +25,23 @@ class MCollectiveTest < Test::Unit::TestCase
     assert @mcollective.run
   end
 
+  def test_run_command_with_mcollective_user_defined
+    @mcollective.stubs(:which).with("sudo", anything).returns("/usr/bin/sudo")
+    @mcollective.stubs(:which).with("mco", anything).returns("/usr/bin/mco")
+    Proxy::Puppet::Plugin.settings.stubs(:mcollective_user).returns("peadmin")
+    @mcollective.expects(:shell_command).with(["/usr/bin/sudo", "-u", "peadmin", "/usr/bin/mco", "puppet", "runonce", "-I", "host1", "host2"]).returns(true)
+    assert @mcollective.run
+  end
+
+  def test_run_command_with_mcollective_user_should_take_precedence
+    @mcollective.stubs(:which).with("sudo", anything).returns("/usr/bin/sudo")
+    @mcollective.stubs(:which).with("mco", anything).returns("/usr/bin/mco")
+    Proxy::Puppet::Plugin.settings.stubs(:puppet_user).returns("example")
+    Proxy::Puppet::Plugin.settings.stubs(:mcollective_user).returns("peadmin")
+    @mcollective.expects(:shell_command).with(["/usr/bin/sudo", "-u", "peadmin", "/usr/bin/mco", "puppet", "runonce", "-I", "host1", "host2"]).returns(true)
+    assert @mcollective.run
+  end
+
   def test_run_command_with_missing_sudo
     @mcollective.stubs(:which).with("sudo", anything).returns(false)
     @mcollective.stubs(:which).with("mco", anything).returns("/usr/bin/mco")
