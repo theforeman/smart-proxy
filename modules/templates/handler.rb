e6c7345f74f6697192858f019dcd8262c5250971
William Hefter <wh@itm-iq.de>
2015-04-23 14:04:47 +0200

    Fixes #10259 - Template proxy does not lookup by MAC address

--- modules/templates/handler.rb
+++ modules/templates/handler.rb
@@ -7,23 +7,25 @@ module Proxy::Templates
   class Handler < ::Proxy::HttpRequest::ForemanRequest
     extend Proxy::Log
 
-    def get_template(kind, token, static = false)
-      opts = { :token => token,
-               :url   => Proxy::Templates::Plugin.settings.template_url
+    def get_template(kind, params, static = false)
+      opts = {
+        :url => Proxy::Templates::Plugin.settings.template_url
       }
+      opts[:mac]    = params[:mac] if params.has_key?('mac')
+      opts[:token]  = params[:token] if params.has_key?('token')
       opts[:static] = static if static
       request = request_factory.create_get("/unattended/#{kind}", opts)
       res = send_request(request)
 
       # You get a 201 from the 'built' URL
-      raise "Error retrieving #{kind} for #{token} from #{uri.host}: #{res.class}" unless ["200", "201"].include?(res.code)
-      Proxy::Log.logger.info "Template: request for #{kind} using #{token} at #{uri.host}"
+      raise "Error retrieving #{kind} for #{opts.to_json} from #{uri.host}: #{res.class}" unless ["200", "201"].include?(res.code)
+      Proxy::Log.logger.info "Template: request for #{kind} using #{opts.to_json} at #{uri.host}"
       res.body
     end
 
-    def self.get_template kind, token, static = false
+    def self.get_template kind, params, static = false
       @handler ||= Handler.new
-      @handler.get_template(kind,token, static)
+      @handler.get_template(kind, params, static)
     end
   end
 
