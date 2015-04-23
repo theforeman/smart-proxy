83fdcd24ffedafb1cc8db57d47ea3eb37c596646
William Hefter <wh@itm-iq.de>
2015-04-23 14:04:47 +0200

    Fixes #10259 - Template proxy does not lookup by MAC address
    Changes by Lukas Zapletal

--- modules/templates/handler.rb
+++ modules/templates/handler.rb
@@ -7,23 +7,27 @@ module Proxy::Templates
   class Handler < ::Proxy::HttpRequest::ForemanRequest
     include Proxy::Log
 
-    def get_template(kind, token, static = false)
-      opts = { :token => token,
-               :url   => Proxy::Templates::Plugin.settings.template_url
+    def get_template(kind, params)
+      opts = {
+        :url => Proxy::Templates::Plugin.settings.template_url
       }
-      opts[:static] = static if static
+      # mac parameter will be only searched when foreman_bootdisk plugin is enabled on the foreman side
+      opts[:mac]    = params[:mac] if params.has_key?(:mac)
+      opts[:token]  = params[:token] if params.has_key?(:token)
+      opts[:static] = params[:static] if params.has_key?(:static)
       request = request_factory.create_get("/unattended/#{kind}", opts)
       logger.debug "retrieving a template from %s/%s" % [Proxy::Templates::Plugin.settings.template_url, request.path]
       res = send_request(request)
 
       # You get a 201 from the 'built' URL
-      raise "Error retrieving #{kind} for #{token} from #{uri.host}: #{res.class}" unless ["200", "201"].include?(res.code)
+      raise "Error retrieving #{kind} for #{opts.inspect} from #{uri.host}: #{res.class}" unless ["200", "201"].include?(res.code)
+      Proxy::Log.logger.info "Template: request for #{kind} using #{opts.inspect} at #{uri.host}"
       res.body
     end
 
-    def self.get_template kind, token, static = false
+    def self.get_template(kind, params)
       @handler ||= Handler.new
-      @handler.get_template(kind,token, static)
+      @handler.get_template(kind, params)
     end
   end
 
--- test/templates/template_test.rb
+++ test/templates/template_test.rb
@@ -17,15 +17,25 @@ class TemplateTest < Test::Unit::TestCase
 
   def test_template_requests_return_data_and_contain_template_url
     @expected_body = "my template"
+    args = { :token => "test-token" }
     stub_request(:get, @foreman_url+'/unattended/provision?token=test-token&url='+@template_url).to_return(:status => [200, 'OK'], :body => @expected_body)
-    result = Proxy::Templates::Handler.get_template('provision', 'test-token')
+    result = Proxy::Templates::Handler.get_template('provision', args)
     assert_equal(@expected_body, result)
   end
 
   def test_template_requests_pass_static_flag_to_foreman
     @expected_body = "my template"
+    args = { :token => "test-token", :static => "true" }
     stub_request(:get, @foreman_url+'/unattended/provision?static=true&token=test-token&url='+@template_url).to_return(:status => [200, 'OK'], :body => @expected_body)
-    result = Proxy::Templates::Handler.get_template('provision', 'test-token', 'true')
+    result = Proxy::Templates::Handler.get_template('provision', args)
+    assert_equal(@expected_body, result)
+  end
+
+  def test_template_requests_with_macaddress
+    @expected_body = "my template"
+    args = { :mac => "aa:bb:cc:dd:ee:ff" }
+    stub_request(:get, @foreman_url+'/unattended/provision?mac=aa:bb:cc:dd:ee:ff&url='+@template_url).to_return(:status => [200, 'OK'], :body => @expected_body)
+    result = Proxy::Templates::Handler.get_template('provision', args)
     assert_equal(@expected_body, result)
   end
 end
