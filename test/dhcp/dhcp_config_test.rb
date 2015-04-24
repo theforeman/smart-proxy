b1109ed10d934802a1f529fc3f5b79c866a89262
Florian Maier <maier@atix.de>
2015-04-24 13:57:18 +0200

    Fixes #10264 - omapi port not evaluated.

--- test/dhcp/dhcp_config_test.rb
+++ test/dhcp/dhcp_config_test.rb
@@ -5,5 +5,6 @@ class DhcpConfigTest < Test::Unit::TestCase
   def test_omitted_settings_have_default_values
     Proxy::DhcpPlugin.load_test_settings({})
     assert_equal 'isc', Proxy::DhcpPlugin.settings.dhcp_provider
+    assert_equal '7911', Proxy::DhcpPlugin.settings.dhcp_omapi_port
   end
 end
