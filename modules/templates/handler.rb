25c06dcb097c10c2d0879eccd370182ebf283101
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2015-06-17 16:41:12 +0100

    fixes #10769 - added a debug trace when downloading templates from foreman

--- modules/templates/handler.rb
+++ modules/templates/handler.rb
@@ -5,7 +5,7 @@ require 'uri'
 module Proxy::Templates
 
   class Handler < ::Proxy::HttpRequest::ForemanRequest
-    extend Proxy::Log
+    include Proxy::Log
 
     def get_template(kind, token, static = false)
       opts = { :token => token,
@@ -13,11 +13,11 @@ module Proxy::Templates
       }
       opts[:static] = static if static
       request = request_factory.create_get("/unattended/#{kind}", opts)
+      logger.debug "retrieving a template from %s/%s" % [Proxy::Templates::Plugin.settings.template_url, request.path]
       res = send_request(request)
 
       # You get a 201 from the 'built' URL
       raise "Error retrieving #{kind} for #{token} from #{uri.host}: #{res.class}" unless ["200", "201"].include?(res.code)
-      Proxy::Log.logger.info "Template: request for #{kind} using #{token} at #{uri.host}"
       res.body
     end
 
