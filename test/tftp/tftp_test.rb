7b1b5967173bb965fae226594ad5a05234a7f4db
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2015-08-03 14:00:33 +0100

    Fixes #11103: tftp tests no longer fail when module config file contains changes.

--- test/tftp/tftp_test.rb
+++ test/tftp/tftp_test.rb
@@ -5,6 +5,7 @@ require "tftp/server"
 class TftpTest < Test::Unit::TestCase
   def setup
     @tftp = Proxy::TFTP::Server.new
+    Proxy::TFTP::Plugin.load_test_settings(:tftproot => "/some/root")
   end
 
   def test_should_have_a_logger
@@ -12,21 +13,20 @@ class TftpTest < Test::Unit::TestCase
   end
 
   def test_path_to_tftp_directory_without_tftproot_setting
+    Proxy::TFTP::Plugin.load_test_settings({})
     assert_equal "/var/lib/tftpboot", @tftp.send(:path)
   end
 
   def test_path_to_tftp_directory_with_tftproot_setting
-    Proxy::TFTP::Plugin.settings.stubs(:tftproot).returns("/some/tftp/root")
-    assert_equal Proxy::TFTP::Plugin.settings.tftproot, @tftp.send(:path)
+    assert_equal "/some/root", @tftp.send(:path)
   end
 
   def test_path_to_tftp_directory_with_relative_tftproot_setting
-    Proxy::TFTP::Plugin.settings.stubs(:tftproot).returns("./some/root")
+    Proxy::TFTP::Plugin.load_test_settings(:tftproot => "./some/root")
     assert_equal Pathname.new(__FILE__).join("..", "..", "..", "modules","tftp","some","root").to_s, @tftp.send(:path)
   end
 
   def test_paths_inside_tftp_directory_dont_raise_errors
-    Proxy::TFTP::Plugin.settings.stubs(:tftproot).returns("/some/root")
     ::Proxy::HttpDownloads.stubs(:start_download).returns(true)
     FileUtils.stubs(:mkdir_p).returns(true)
 
@@ -34,7 +34,6 @@ class TftpTest < Test::Unit::TestCase
   end
 
   def test_paths_outside_tftp_directory_raise_errors
-    Proxy::TFTP::Plugin.settings.stubs(:tftproot).returns("/some/root")
     ::Proxy::HttpDownloads.stubs(:start_download).returns(true)
     FileUtils.stubs(:mkdir_p).returns(true)
 
