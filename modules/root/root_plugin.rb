b36a8423a6b6c09e3fc10eb0af823a5df282b10d
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2015-10-13 12:23:05 +0100

    Fixes #9974: do not attempt to open settings.d/foreman_proxy.yml
      'root' module (aka 'foreman_proxy') doesn't have config file and will no longer try to open it on startup.

--- modules/root/root_plugin.rb
+++ modules/root/root_plugin.rb
@@ -4,4 +4,9 @@ class ::Proxy::RootPlugin < ::Proxy::Plugin
 
   http_rackup_path File.expand_path("http_config.ru", File.expand_path("../", __FILE__))
   https_rackup_path File.expand_path("http_config.ru", File.expand_path("../", __FILE__))
+
+  # this is a special case: 'root' module doesn't have configuration file
+  def self.settings
+    @settings ||= ::Proxy::Settings::Plugin.new(plugin_default_settings, {})
+  end
 end
