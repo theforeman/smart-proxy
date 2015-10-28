108d698c511267f0bf355e054f5321d4e4c703b7
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2015-10-28 14:20:13 +0000

    Fixes #12254: pinned addressable gem to ~> 2.3.8
    This is the last version to support 1.8.7.
    
    (cherry picked from commit 248751937b99947a00c5d21a47f2527327b263da)

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -6,6 +6,7 @@ group :test do
   gem 'ci_reporter', '>= 1.6.3', "< 2.0.0", :require => false
   gem 'rdoc'
   gem 'minitest', '~> 4.7', :platforms => :ruby_19
+  gem 'addressable', '~> 2.3.8' if RUBY_VERSION == '1.8.7' # 2.4.0 drops support for ruby 1.8.7
   gem 'webmock'
   gem 'rubocop-checkstyle_formatter' if RUBY_VERSION > "1.9.2"
 end
