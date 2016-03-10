6059f2d3f22def98743218788c08603541df13c5
Dominic Cleal <dominic@cleal.org>
2016-03-10 08:46:50 +0000

    fixes #14134 - update rubocop and pin for rake 11.x compatibility

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -1,6 +1,5 @@
 group :test do
   gem 'mocha', '> 0.13.0', :require => false
-  gem 'rake'
   gem 'rack-test'
   gem 'single_test'
   gem 'ci_reporter', '>= 1.6.3', "< 2.0.0", :require => false
@@ -8,5 +7,11 @@ group :test do
   gem 'test-unit' if RUBY_VERSION > "1.8.7"
   gem 'addressable', '~> 2.3.8' if RUBY_VERSION == '1.8.7' # 2.4.0 drops support for ruby 1.8.7
   gem 'webmock'
-  gem 'rubocop-checkstyle_formatter', '~> 0.2' if RUBY_VERSION > "1.9.2"
+
+  if RUBY_VERSION < '1.9.3'
+    gem 'rake', '< 11'
+  else
+    gem 'rake'
+    gem 'rubocop-checkstyle_formatter', '~> 0.2'
+  end
 end
