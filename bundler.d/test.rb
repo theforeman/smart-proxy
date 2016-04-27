f5a8322bba3f8122bf845a1dac2c247a5ea68f1c
Michael Moll <kvedulv@kvedulv.de>
2016-04-28 00:33:46 +0200

    Fixes #14862 - pin webmock version for Ruby <1.9.3
    (cherry picked from commit cfb732db04b0bdbee87f2d104329cf5b45670ac5)

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -6,7 +6,12 @@ group :test do
   gem 'rdoc'
   gem 'minitest', '~> 4.7', :platforms => :ruby_19
   gem 'addressable', '~> 2.3.8' if RUBY_VERSION == '1.8.7' # 2.4.0 drops support for ruby 1.8.7
-  gem 'webmock'
   gem 'rubocop-checkstyle_formatter' if RUBY_VERSION > "1.9.2"
   gem 'rake', '< 11'
+
+  if RUBY_VERSION < '1.9.3'
+    gem 'webmock', '< 2.0.0'
+  else
+    gem 'webmock'
+  end
 end
