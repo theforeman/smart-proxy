4123673ccba1ec239e8c32db0c605b57a2f4e842
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2016-04-29 16:19:07 +0100

    Fixes #14775 - virsh provider is now being renamed
      When migrating during dns and dhcp configuration files.

--- test/migrations/libvirt_migration_test.rb
+++ test/migrations/libvirt_migration_test.rb
@@ -8,23 +8,37 @@ class ProxyLibvirtMigrationTest < Test::Unit::TestCase
   def setup
     @old_config = YAML.load_file(File.join(File.dirname(__FILE__),'./migration_settings.yml'))
     @migration = MigrateVirshToLibvirtConfig.new("/tmp")
-    @dhcp_data = @migration.transform_dhcp_yaml(@old_config.dup)
-    @dns_data = @migration.transform_dns_yaml(@old_config.dup)
+  end
+
+  def test_transform_main_dhcp_configuration
+    assert_equal({:use_provider => 'dhcp_libvirt'}, @migration.transform_dhcp_yaml(:use_provider => 'dhcp_virsh'))
+  end
+
+  def test_transform_main_dhcp_configuration_when_another_provider_is_used
+    assert_equal({:use_provider => 'dhcp_isc'}, @migration.transform_dhcp_yaml(:use_provider => 'dhcp_isc'))
   end
 
   def test_transform_dhcp_yaml_empty
-    assert_equal 'default', @migration.transform_dhcp_yaml({})[:network]
+    assert_equal 'default', @migration.transform_dhcp_libvirt_yaml({})[:network]
   end
 
   def test_output_has_correct_dhcp_network
-    assert_equal 'mynetwork', @dhcp_data[:network]
+    assert_equal 'mynetwork', @migration.transform_dhcp_libvirt_yaml(@old_config.dup)[:network]
   end
 
   def test_transform_dns_yaml_empty
-    assert_equal 'default', @migration.transform_dns_yaml({})[:network]
+    assert_equal 'default', @migration.transform_dns_libvirt_yaml({})[:network]
+  end
+
+  def test_transform_main_dns_configuration
+    assert_equal({:use_provider => 'dns_libvirt'}, @migration.transform_dns_yaml(:use_provider => 'dns_virsh'))
+  end
+
+  def test_transform_main_dns_configuration_when_another_provider_is_used
+    assert_equal({:use_provider => 'dns_nsupdate'}, @migration.transform_dns_yaml(:use_provider => 'dns_nsupdate'))
   end
 
   def test_output_has_correct_dns_network
-    assert_equal 'mynetwork', @dns_data[:network]
+    assert_equal 'mynetwork', @migration.transform_dns_libvirt_yaml(@old_config.dup)[:network]
   end
 end
