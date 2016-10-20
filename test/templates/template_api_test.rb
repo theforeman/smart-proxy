3b81b1d90378e23bc10f9100c6d8f243379894f0
Lukas Zapletal <lzap+git@redhat.com>
2016-10-20 14:39:18 +0200

    Fixes #17045 - hostgroup templates URLs proxied correctly

--- test/templates/template_api_test.rb
+++ test/templates/template_api_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 require 'json'
 require 'templates/templates_api'
 require 'templates/templates'
+require 'webmock/test_unit'
 
 class TemplateApiTest < Test::Unit::TestCase
   include Rack::Test::Methods
@@ -11,23 +12,38 @@ class TemplateApiTest < Test::Unit::TestCase
   end
 
   def setup
-    Proxy::Templates::Plugin.settings.stubs(:template_url).returns("someproxy:8443")
-    Proxy::SETTINGS.stubs(:foreman_url).returns("someproxy:8443")
+    @foreman_url = 'https://foreman.example.com'
+    Proxy::SETTINGS.stubs(:foreman_url).returns(@foreman_url)
+    @template_url = 'http://proxy.lan:8443'
+    Proxy::Templates::Plugin.settings.stubs(:template_url).returns(@template_url)
     @args = { :token => "test-token" }
   end
 
   def test_api_can_return_templateserver
     get "/templateServer"
-    assert last_response.ok?, "Last response was not ok"
+    assert last_response.ok?, "Last response was ok"
     data = JSON.parse(last_response.body)
-    assert_equal("someproxy:8443", data["templateServer"].to_s)
+    assert_equal(@template_url, data["templateServer"].to_s)
   end
 
-  def test_api_can_ask_for_a_template
-    Proxy::Templates::TemplateProxyRequest.any_instance.expects(:get_template).returns("An API template")
+  def test_api_passes_token_and_template_url
+    stub_request(:get, "#{@foreman_url}/unattended/provision").with(query: {"token" => "test-token", "url" => @template_url}).to_return(:body => 'An API template')
     get "/provision", @args
-    assert last_response.ok?, "Last response was not ok"
+    assert last_response.ok?, "Last response was ok"
     assert_match("An API template", last_response.body)
   end
 
+  def test_api_can_ask_for_a_template
+    stub_request(:get, "#{@foreman_url}/unattended/provision").with(query: {"url" => @template_url}).to_return(:body => 'An API template')
+    get "/provision", {}
+    assert last_response.ok?, "Last response was ok"
+    assert_match("An API template", last_response.body)
+  end
+
+  def test_api_can_ask_for_a_hostgroup_template
+    stub_request(:get, "#{@foreman_url}/unattended/kind/temp/hg").with(query: {"url" => @template_url}).to_return(:body => 'An API template')
+    get "/kind/temp/hg", {}
+    assert last_response.ok?, "Last response was ok"
+    assert_match("An API template", last_response.body)
+  end
 end
