2669bbc7adb65f6a729c4c59b6548e3be896a4d8
Dominic Cleal <dominic@cleal.org>
2016-11-17 13:58:05 +0000

    Benchmark for #find_subnet performance
    Adds 200 hosts to each /24 subnet and runs find_subnet for each host.
    
    Calculating -------------------------------------
    find_subnet (1 subnets, 200 hosts)
                            612.695  (±14.4%) i/s -      5.672k in   9.972658s
    find_subnet (5 subnets, 1000 hosts)
                             99.585  (±11.0%) i/s -    980.000  in   9.995022s
    find_subnet (50 subnets, 10000 hosts)
                              9.239  (±10.8%) i/s -     92.000  in  10.082209s
    find_subnet (500 subnets, 100000 hosts)
                              0.816  (± 0.0%) i/s -      9.000  in  11.036854s
    find_subnet (5000 subnets, 1000000 hosts)
                              0.068  (± 0.0%) i/s -      1.000  in  14.690730s
    
    (cherry picked from commit c3cb604b281ffa9584a4ccbd17f7c973b214f5dc)

---
+++ test/dhcp/subnet_service_find_subnet_benchmark.rb
@@ -0,0 +1,37 @@
+require 'benchmark_helper'
+
+require 'dhcp_common/dhcp_common'
+require 'dhcp_common/subnet'
+require 'dhcp_common/subnet_service'
+
+host_count = 200
+
+proxy_benchmark do
+  Benchmark.ips do |x|
+    x.config(:time => 10, :warmup => 0)
+
+    [1, 5, 50, 500, 5000].each do |subnet_count|
+      hosts = []
+      s1 = s2 = 0
+      subnets = (1..subnet_count).map do |i|
+        s2 += 1
+        if s2 % 256 == 0
+          s1 += 1
+          s2 = 0
+        end
+
+        prefix = "#{s1}.#{s2}.0"
+        netmask = '255.255.255.0'
+        host_count.times { |i| hosts << "#{prefix}.#{i}" }
+        Proxy::DHCP::Subnet.new(prefix + '.0', netmask, {})
+      end
+
+      service = Proxy::DHCP::SubnetService.initialized_instance
+      subnets.each { |s| service.add_subnet(s) }
+
+      x.report("find_subnet (#{subnet_count} subnets, #{host_count * subnet_count} hosts)") do
+        hosts.each { |s| service.find_subnet(s) }
+      end
+    end
+  end
+end
