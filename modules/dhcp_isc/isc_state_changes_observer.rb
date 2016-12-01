a04c4f22b312d88dadfcd4e937156040ac0aac96
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2016-12-01 17:09:49 +0000

    Fixes #17554 - inotify events can't overflow the observer anymore

--- modules/dhcp_isc/isc_state_changes_observer.rb
+++ modules/dhcp_isc/isc_state_changes_observer.rb
@@ -3,15 +3,125 @@ module Proxy::DHCP
     class IscStateChangesObserver
       include ::Proxy::Log
 
-      attr_reader :service, :leases_file, :config_file
+      class Events
+        attr_accessor :m, :last_event
 
-      def initialize(config_file, leases_file, subnet_service)
+        STARTED = :started
+        STOPPED = :stopped
+        MODIFIED = :modified
+        RECREATED = :recreated
+        NONE = :none
+
+        def initialize
+          @m = Monitor.new
+          @last_event = NONE
+        end
+
+        def started
+          push(STARTED)
+        end
+
+        def stopped
+          push(STOPPED)
+        end
+
+        def modified
+          push(MODIFIED)
+        end
+
+        def recreated
+          push(RECREATED)
+        end
+
+        def push(an_event)
+          m.synchronize do
+            case an_event
+            when STARTED
+              return false unless @last_event == NONE
+              @last_event = STARTED
+            when STOPPED
+              @last_event = STOPPED
+            when MODIFIED
+              return false unless @last_event == NONE
+              @last_event = MODIFIED
+            when RECREATED
+              return false if @last_event != NONE && @last_event != MODIFIED
+              @last_event = RECREATED
+            else
+              return false
+            end
+          end
+          return true
+        end
+
+        def pop
+          @m.synchronize do
+            to_return = @last_event
+            @last_event = NONE
+            to_return
+          end
+        end
+      end
+
+      attr_reader :service, :leases_file, :config_file, :events, :event_loop_active, :worker
+
+      def initialize(config_file, leases_file, subnet_service, events = Events.new)
         @config_file = config_file
         @leases_file = leases_file
         @service = subnet_service
+        @events = events
+        @event_loop_active = false
       end
 
-      def monitor_started
+      def start
+        do_start
+        @event_loop_active = true
+        @worker = new_worker
+        nil
+      end
+
+      def stop
+        events.stopped
+        worker.wakeup unless worker.nil?
+      end
+
+      def leases_modified
+        raise "IscStateChangesObserver worker thread hasn't been started" if worker.nil?
+        events.modified
+        worker.wakeup
+      end
+
+      def leases_recreated
+        raise "IscStateChangesObserver worker thread hasn't been started" if worker.nil?
+        events.recreated
+        worker.wakeup
+      end
+
+      def event_loop
+        while event_loop_active
+          case events.pop
+          when Events::MODIFIED
+            do_leases_modified
+          when Events::RECREATED
+            do_leases_recreated
+          when Events::STOPPED
+            do_stop
+            @event_loop_active = false
+          else
+            pause
+          end
+        end
+      end
+
+      def new_worker
+        Thread.new { event_loop }
+      end
+
+      def pause
+        Thread.stop
+      end
+
+      def do_start
         service.group_changes do
           load_subnets
           update_subnet_service_with_dhcp_records(config_file.hosts_and_leases)
@@ -19,11 +129,11 @@ module Proxy::DHCP
         end
       end
 
-      def leases_modified
+      def do_leases_modified
         service.group_changes { update_subnet_service_with_dhcp_records(leases_file.hosts_and_leases) }
       end
 
-      def leases_recreated
+      def do_leases_recreated
         service.group_changes do
           config_file.close rescue nil
           leases_file.close rescue nil
@@ -36,7 +146,7 @@ module Proxy::DHCP
         end
       end
 
-      def monitor_stopped
+      def do_stop
         config_file.close rescue nil
         leases_file.close rescue nil
       end
