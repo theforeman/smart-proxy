5e864aa8e699657169f7e1426ee51eafb65a5f65
Mårten Cassel <marten.cassel@gmail.com>
2017-02-13 09:56:38 +0100

    Add requie for realm_ad in proxy main

--- modules/realm_ad/realm_ad_plugin.rb
+++ modules/realm_ad/realm_ad_plugin.rb
@@ -1,7 +1,7 @@
 module Proxy::ADRealm
   class Plugin < Proxy::Provider
     load_classes ::Proxy::ADRealm::ConfigurationLoader
-    load_depedency_injection_wirings ::Proxy::ADRealm::ConfigurationLoader
+    load_dependency_injection_wirings ::Proxy::ADRealm::ConfigurationLoader
 
     validate_presence :keytab_path, :principal
     validate_readable :keytab_path
