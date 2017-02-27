d1e6c33b471010e590f5a146efad64ddba8212e9
martencassel <marten.cassel@gmail.com>
2017-02-27 14:19:45 +0100

    more realm_ad provider code. planned tests

--- modules/realm_ad/provider.rb
+++ modules/realm_ad/provider.rb
@@ -16,69 +16,85 @@ module Proxy::ADRealm
       @ldap_user = ldap_user
       @ldap_password = ldap_password
       @ldap_port = ldap_port
+      @domain = @realm.downcase
 
-      # Get krb5 token
+      # Connec to active directory
       init_krb5_ccache @keytab_path, @principal
+      @adconn = radcli_connect
     end
 
     def check_realm realm
-    end
-
-    def find hostname
+      raise Exception.new "Unknown realm #{realm}" unless realm.casecmp(@realm).zero?
     end
 
     def create realm, hostname, params
       check_realm realm
-    end
 
-    def do_host_rebuild(hostname)
-    end
+      begin
+        if params[:rebuild] == "true"
+          otp = generate_random_password()
+          radcli_reset(@adconn, otp)
+          result = {:randompassword => otp}
+        else
+          otp = generate_random_password()
+          radcli_password(@adconn, otp)
+          result = {:randompassword => otp}
+        end
+      rescue AdEnroll::Exception  => e
+        raise
+      end
 
-    def do_host_modify(hostname)
-    end
+      JSON.pretty_generate(result)
 
-    def do_host_create(hostname)
     end
 
     def delete realm, hostname
       check_realm realm
+
+      begin
+        radcli_delete(@adconn, hostname)
+      rescue AdEnroll::Exception => else
+        raise
+      end
+    end
+
+    def generate_random_password
+      return "_i7@PhgpAnjn"
     end
 
     private
 
-    def ldap_host_exists? hostname
-      ldap = Net::LDAP.new
-      ldap.host = @domain_controller 
-      ldap.port = @ldap_port
-      ldap.auth @ldap_user, @ldap_password
-      filter = Net::LDAP::Filter.eq( "DNSHostname", hostname )
-      treebase = domainname_to_basedn @realm
-      if ldap.bind 
-        ldap.search( :base => treebase, :filter => filter) do |entry|
-          if entry == nil
-            logger.debug "Host with DNSName #{hostname} was not found in domain"
-            return false
-          else 
-            logger.debug "Found Host in domain for DNSName #{hostname}"         
-            logger.debug "LDAP Returned DN: #{entry.dn}"
-            return true
-          end
-        end
-      else
-        logger.debug "Authentication failed"
-        ldap.get_operation_result
-        return false
-      end
-      return false 
-    end 
-
-    def radcli_join computer_name, password
+    def radcli_connect 
+      # Connect to active directory
+      conn = Adcli::AdConn.new(@domain)
+      conn.set_domain_realm(@realm)
+      conn.set_domain_controller(@domain_controller)
+      conn.set_login_ccache_name("")
+      conn.connect()
+      return conn
     end
 
-    def radcli_reset computer_name, newpassword
+    def radcli_join computer_name, password
+      # Join computer
+      enroll = Adcli::AdEnroll.new(@adconn)
+      enroll.set_computer_name(computer_name)
+      enroll.set_computer_password(password)
+      enroll.join()
+    end
+
+    def radcli_password computer_name
+      # Reset a computer's password
+      enroll = Adcli::AdEnroll.new(@adconn)
+      enroll.set_computer_name(computer_name)
+      enroll.set_computer_name(computer_name)
+      enroll.password()
     end
 
     def radcli_delete computer_name
+      # Delete a computer's account
+      enroll = Adcli::AdEnroll.new(@adconn)
+      enroll.set_computer_name(computer_name)
+      enroll.delete()
     end
   end
 end
--- test/realm/ad_provider_test.rb
+++ test/realm/ad_provider_test.rb
@@ -3,33 +3,31 @@ require 'realm_ad/provider'
 
 class RealmADTest < Test::Unit::TestCase
   def setup
-    @provider = Proxy::ADRealm::Provider.new('realm', 'keytab', 'principal', 'domain-controller', 'ldap-user', 'ldap-password')
+    @realm = 'test_realm'
+    @provider = Proxy::ADRealm::Provider.new(@realm, 'keytab_path', 'principal', 'domain-controller', 'ldap-user', 'ldap-password', 'ldap-port')
   end
 
-  def test_find
-    # private ldap_find returns a hostname 'a_host'
-    @provider.find('a_host')
-  end
+  # New host. hostname is set, rebuild is null. radcli_join doesnt raise exception.
+  # create method returns json of otp. otp function returns _i7@PhgpAnjn.
 
-  def test_find_if_host_does_not_exist
-    # private ldap_find throws exception with message 'not found'
-    assert_nil @provider.find('a_host')
-  end
- 
+  # New host. hostname is set. rebuild is not specified. radcli_join raises an exception.
+  # create method doesnt return json, propagates exception back to caller that return error 400.
+
+  # Rebuild host. hostname is not null. rebuild is set. radcli_reset doesnt raise exception.
+  # create method return json of otp. otp function returns Nx7$Av12Aja_ string.
+
+  # Rebuild host. hostname is set. rebuild is set. radcli_reset raises an exception.
+  # create method doesnt return json, propagates exception back to caller that return error 400.
+
+  # Rebuild host. host account doesnt exist. radcli_join throws an "not found" exception. Returns 404 if not found.
+  # Rebuild host, reset password. radcli_password throws an "Not found" exception". Return 404 if not found.
+
+  # Delete host. hostname is not null. radcli_delete doesnt raise exception. delete returns nothing.
+  # Delete host. radcli_delete raises an exception. delete returns nothing.
+  # Delete host. radcli_delete throws "not found" exception.  delete returns 404 error.
+
   def test_delete
-    # private radcli_delete with 'a_host' return json with hostname
-  end
-
-  def test_delete_with_unrecognized_realm_raises_exception
-    # raises exception, @provider.delete('unkown_realm', 'a_host')
-  end
-
-  def test_create_host
-  end
-
-  def test_create_host_unrecognized_realm_raises_exception
-  end
-
-  def test_create_raises_exception_on_error
+    ok_result = {:a => 'a'}
+    @provider.expects()
   end
 end
