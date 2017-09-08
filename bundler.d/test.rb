cb3274ce51ca3ef53d19b5f1e5595f96451ac71d
Michael Moll <kvedulv@kvedulv.de>
2017-09-08 12:02:21 +0200

    Refs #20843 - Put public_suffix only in test
    public_suffix is only a dependency for webmock

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -6,6 +6,7 @@ group :test do
   gem 'rdoc'
   gem 'test-unit' if RUBY_VERSION > "1.8.7"
   gem 'addressable', '~> 2.3.8' if RUBY_VERSION == '1.8.7' # 2.4.0 drops support for ruby 1.8.7
+  gem 'public_suffix', '< 3' if RUBY_VERSION < '2.1'
 
   if RUBY_VERSION < '1.9.3'
     gem 'rake', '< 11'
