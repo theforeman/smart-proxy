1075f492ab6c5ba5419b6c61f151f692f0332543
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2017-11-21 14:24:20 -0800

    Fixes #21729 - removed gem specs for older rubies

--- bundler.d/puppet.rb
+++ bundler.d/puppet.rb
@@ -1,5 +1,4 @@
 group :puppet_proxy_legacy do
-  gem 'json_pure', '< 2.0.0', :require => false if RUBY_VERSION < '2.0.0'
   gem 'puppet', '< 5.0.0'
   gem 'ruby-augeas', :require => 'augeas'
 end
--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -2,21 +2,11 @@ group :test do
   gem 'mocha', '> 0.13.0', :require => false
   gem 'single_test'
   gem 'ci_reporter', '>= 1.6.3', "< 2.0.0", :require => false
-  gem 'test-unit' if RUBY_VERSION > "1.8.7"
-  gem 'addressable', '~> 2.3.8' if RUBY_VERSION == '1.8.7' # 2.4.0 drops support for ruby 1.8.7
+  gem 'test-unit'
   gem 'public_suffix', '< 3' if RUBY_VERSION < '2.1'
   gem 'benchmark-ips'
   gem 'ruby-prof'
-  gem 'rubocop', '0.38.0' if RUBY_VERSION > "1.9.2"
-
-  if RUBY_VERSION < '1.9.3'
-    gem 'rake', '< 11'
-    gem 'webmock', '< 2.0.0'
-  else
-    gem 'rake'
-    gem 'rubocop-checkstyle_formatter', '~> 0.2'
-    gem 'webmock'
-  end
+  gem 'rubocop', '0.38.0'
 
   if RUBY_VERSION < '2.2.2'
     gem 'rack-test', '~> 0.7.0'
@@ -29,4 +19,8 @@ group :test do
   else
     gem 'rdoc'
   end
+  
+  gem 'rake'
+  gem 'rubocop-checkstyle_formatter', '~> 0.2'
+  gem 'webmock'
 end
