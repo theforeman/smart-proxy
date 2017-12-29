61e025b5045fa6c2014b2544fea2f8946e98e029
Michael Moll <kvedulv@kvedulv.de>
2017-12-29 19:04:53 +0100

    Fixes #22098 - pin rdoc gem for Ruby <2.2

--- bundler.d/development.rb
+++ bundler.d/development.rb
@@ -2,7 +2,12 @@ group :development do
   # To use debugger
   gem "ruby-debug", :platforms => :ruby_18
   gem "ruby-debug19", :platforms => :ruby_19
-  gem 'rdoc'
   gem 'single_test'
   gem 'pry'
+
+  if RUBY_VERSION < '2.2'
+    gem 'rdoc', '< 6'
+  else
+    gem 'rdoc'
+  end
 end
