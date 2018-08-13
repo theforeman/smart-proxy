e7d9d2baa1dfcef1c5183088a7d310619fa67acd
Lukas Zapletal <lzap+git@redhat.com>
2018-08-13 10:09:32 +0200

    Fixes #24591 - fixed debug message in MSDHCP

--- modules/dhcp_native_ms/dhcp_native_ms_main.rb
+++ modules/dhcp_native_ms/dhcp_native_ms_main.rb
@@ -193,7 +193,7 @@ module Proxy::DHCP::NativeMS
 
       to_return
     rescue Exception
-      logger.debug("Skipping a reservation as it failed validation: '%s'" % [opts.inspect])
+      logger.debug("Skipping a reservation as it failed validation: '%s'" % [options.inspect])
       nil
     end
 
@@ -221,7 +221,7 @@ module Proxy::DHCP::NativeMS
       logger.debug to_return.inspect
       to_return
     rescue Exception
-      logger.debug("Skipping a lease as it failed validation: '%s'" % [opts.inspect])
+      logger.debug("Skipping a lease as it failed validation: '%s'" % [options.inspect])
       nil
     end
 
