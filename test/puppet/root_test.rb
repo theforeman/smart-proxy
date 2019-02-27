2f9521bc7d5977073cbdbcfad3f0435709e922fd
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-02-27 19:39:25 +0100

    Refs #25614 - Add integration tests for capabilities

---
+++ test/puppet/root_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+require 'json'
+require 'root/root_v2_api'
+require 'puppet_proxy/puppet'
+require 'puppet_proxy_puppet_api/puppet_proxy_puppet_api'
+
+class PuppetApiFeaturesTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Proxy::PluginInitializer.new(Proxy::Plugins.instance).initialize_plugins
+    Proxy::RootV2Api.new
+  end
+
+  def test_features
+    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('puppet.yml').returns({enabled: true, puppet_version: '5.5.8'})
+    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('puppet_proxy_puppet_api.yml').returns({puppet_url: 'https://puppet.example.com:8140'})
+
+    get '/features'
+
+    response = JSON.parse(last_response.body)
+
+    mod = response['puppet']
+    refute_nil(mod)
+    assert_equal([], mod['capabilities'])
+    assert_equal({}, mod['settings'])
+  end
+end
---
+++ test/puppetca_http_api/root_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+require 'json'
+require 'root/root_v2_api'
+require 'puppetca/puppetca'
+require 'puppetca_hostname_whitelisting/puppetca_hostname_whitelisting'
+require 'puppetca_http_api/puppetca_http_api'
+
+class PuppetcaApiFeaturesTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Proxy::PluginInitializer.new(Proxy::Plugins.instance).initialize_plugins
+    Proxy::RootV2Api.new
+  end
+
+  def test_features
+    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('puppetca.yml').returns({enabled: true, puppet_version: '6.0.0'})
+    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('puppetca_hostname_whitelisting.yml').returns({})
+    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('puppetca_http_api.yml').returns({puppet_url: 'https://puppet.example.com:8140'})
+
+    get '/features'
+
+    response = JSON.parse(last_response.body)
+
+    mod = response['puppetca']
+    refute_nil(mod)
+    assert_equal([], mod['capabilities'])
+    assert_equal({}, mod['settings'])
+  end
+
+end
