293a77a11f86494528a8ca181dca97e9a37be4dc
Lukas Zapletal <lzap+git@redhat.com>
2019-03-13 10:03:34 +0100

    Fixes #26329 - TFTP not needed active for HTTPBoot

--- modules/httpboot/httpboot_plugin.rb
+++ modules/httpboot/httpboot_plugin.rb
@@ -4,7 +4,6 @@ module Proxy::Httpboot
     https_rackup_path File.expand_path("http_config.ru", File.expand_path("../", __FILE__))
 
     plugin :httpboot, ::Proxy::VERSION
-    requires :tftp, ::Proxy::VERSION
 
     default_settings :root_dir => '/var/lib/tftpboot'
   end
