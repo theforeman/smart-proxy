fbffe8082a5d7546a705759d5179f1ee33bfb73e
Michael Moll <kvedulv@kvedulv.de>
2019-05-04 17:25:58 +0200

    Fixes #26742 - use Ruby 2.0 compatible gem versions

--- bundler.d/dhcp_isc.rb
+++ bundler.d/dhcp_isc.rb
@@ -1,6 +1,10 @@
 group :dhcp_isc_inotify do
   gem 'rsec'
-  gem 'rb-inotify'
+  if RUBY_VERSION < '2.2'
+    gem 'rb-inotify', '< 0.10'
+  else
+    gem 'rb-inotify'
+  end
 end
 
 group :dhcp_isc_kqueue do
