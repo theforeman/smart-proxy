d5f5bca0ac2ea5815df45b06c68ac0829db9bc1e
Lukas Zapletal <lzap+git@redhat.com>
2019-06-12 17:32:09 +0200

    Fixes #27036 - improve logging of error backtraces

--- modules/dhcp_libvirt/subnet_service_initializer.rb
+++ modules/dhcp_libvirt/subnet_service_initializer.rb
@@ -37,8 +37,8 @@ module Proxy::DHCP::Libvirt
       raise Proxy::DHCP::Error("Only one subnet is supported") if ret_val.size > 1
       ret_val
     rescue Exception => e
-      msg = "Unable to parse subnets XML"
-      logger.error msg, e
+      msg = "Unable to parse subnets XML: #{e}"
+      logger.exception msg, e
       logger.debug xml if defined?(xml)
       raise Proxy::DHCP::Error, msg
     end
@@ -58,8 +58,8 @@ module Proxy::DHCP::Libvirt
       end
       to_ret
     rescue Exception => e
-      msg = "Unable to parse reservations XML"
-      logger.error msg, e
+      msg = "Unable to parse reservations XML: #{e}"
+      logger.exception msg, e
       logger.debug xml if defined?(xml)
       raise Proxy::DHCP::Error, msg
     end
