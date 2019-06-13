b54c6bd4f38039f7c80c586edf960aaba0c40ba5
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-06-13 14:52:06 +0200

    Fixes #27047 - Remove dns_find and clean up ptr_record_conflicts
    78da2d1ef03ac150812226946ac1ec546838624c (1.14) deprecated dns_find.
    bfbdeeb90e344ee8b0c9950f85b159035d9cbfbe (1.16) deprecated passing IPs
    to ptr_record_conflicts in 1.16. There was no clear deprecation
    timeline, but sufficient time has passed to remove them. I've manually
    verified the DNS plugins under theforeman organization and I'm not aware
    of other implementations.

--- test/dns_common/record_test.rb
+++ test/dns_common/record_test.rb
@@ -6,21 +6,6 @@ class DnsRecordTest < Test::Unit::TestCase
     @record = Proxy::Dns::Record.new
   end
 
-  def test_dns_find_with_ip_parameter
-    @record.expects(:get_name).with('2.0.13.127.in-addr.arpa').returns('not_existing.example.com')
-    assert_equal 'not_existing.example.com', @record.dns_find('2.0.13.127.in-addr.arpa')
-  end
-
-  def test_dns_find_with_ipv6_parameter
-    @record.expects(:get_name).with('1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.f.e.e.d.8.b.d.0.1.0.0.2.ip6.arpa').returns('not_existing.example.com')
-    assert_equal 'not_existing.example.com', @record.dns_find('1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.f.e.e.d.8.b.d.0.1.0.0.2.ip6.arpa')
-  end
-
-  def test_dns_find_with_fqdn_parameter
-    Resolv::DNS.any_instance.expects(:getaddress).with('some.host').returns(Resolv::IPv4.create('127.13.0.2'))
-    assert_equal '127.13.0.2', Proxy::Dns::Record.new.dns_find('some.host')
-  end
-
   def test_get_name_with_sideeffect_for_ipv4
     @record.expects(:get_resource_as_string!).with('2.0.13.127.in-addr.arpa', Resolv::DNS::Resource::IN::PTR, :name).returns('not_existing.example.com')
     assert 'not_existing.example.com', @record.get_name!('2.0.13.127.in-addr.arpa')
@@ -81,11 +66,6 @@ class DnsRecordTest < Test::Unit::TestCase
     assert_false @record.get_resource_as_string('some.host', Resolv::DNS::Resource::IN::A, :address)
   end
 
-  def test_dns_find_key_not_found
-    Resolv::DNS.any_instance.expects(:getaddress).with('another.host').raises(Resolv::ResolvError.new('DNS result has no information'))
-    assert !Proxy::Dns::Record.new.dns_find('another.host')
-  end
-
   def test_ptr_to_ip_ipv4
     assert_equal('192.168.33.30', Proxy::Dns::Record.new.ptr_to_ip('30.33.168.192.in-addr.arpa'))
   end
@@ -128,22 +108,6 @@ class DnsRecordTest < Test::Unit::TestCase
   def test_aaaa_record_conflicts_but_nothing_todo
     Resolv::DNS.any_instance.expects(:getresources).with('some.host',  Resolv::DNS::Resource::IN::AAAA).returns(ips('2001:DB8:DEEF::1'))
     assert_equal 0, Proxy::Dns::Record.new.aaaa_record_conflicts('some.host', '2001:DB8:DEEF::1')
-  end
-
-  def test_old_ptr_record_conflicts_no_conflict
-    Resolv::DNS.any_instance.expects(:getresources).with('33.33.168.192.in-addr.arpa',  Resolv::DNS::Resource::IN::PTR).returns([])
-    assert_equal -1, Proxy::Dns::Record.new.ptr_record_conflicts('some.host', '192.168.33.33')
-  end
-
-  def test_old_ptr_record_conflicts_has_conflict
-    Resolv::DNS.any_instance.expects(:getresources).with('1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.f.e.e.d.8.b.d.0.1.0.0.2.ip6.arpa',
-                                                         Resolv::DNS::Resource::IN::PTR).returns([Resolv::DNS::Resource::IN::PTR.new('some.host')])
-    assert_equal 1, Proxy::Dns::Record.new.ptr_record_conflicts('another.host', '2001:db8:deef::1')
-  end
-
-  def test_old_ptr_record_conflicts_but_nothing_todo
-    Resolv::DNS.any_instance.expects(:getresources).with('33.33.168.192.in-addr.arpa',  Resolv::DNS::Resource::IN::PTR).returns([Resolv::DNS::Resource::IN::PTR.new('some.host')])
-    assert_equal 0, Proxy::Dns::Record.new.ptr_record_conflicts('some.host', '192.168.33.33')
   end
 
   def test_ptr_record_conflicts_no_conflict
