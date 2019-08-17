e2fc9de6691a37acecc535bd678648e77d60dc2c
Nathan Dietsch <njd@ndietsch.com>
2019-08-16 22:43:34 -0500

    fixes #27649 - anonymises organisation specific server references
    These two test files contain referneces to organisation specific files and this pull request anonymises them. Please feel free to change someserver.somedomain to any other value.

--- test/dhcp/sparc_attrs.rb
+++ test/dhcp/sparc_attrs.rb
@@ -1,7 +1,7 @@
 module SparcAttrs
   def sparc_attrs
     {
-        "hostname"=>"itgsyddev910.macbank",
+        "hostname"=>"someserver.somedomain",
         "mac"=>"00:21:28:6d:62:e8",
         "ip"=>"192.168.122.11",
         "network"=>"192.168.122.0",
@@ -11,8 +11,8 @@ module SparcAttrs
         "<SPARC-Enterprise-T5120>sysid_server_path"=>"192.168.122.24:/Solaris/jumpstart/sysidcfg/sysidcfg_primary",
         "<SPARC-Enterprise-T5120>install_server_ip"=>"192.168.122.24",
         "<SPARC-Enterprise-T5120>jumpstart_server_path"=>"192.168.122.24:/Solaris/jumpstart",
-        "<SPARC-Enterprise-T5120>install_server_name"=>"itgsyddev807.macbank",
-        "<SPARC-Enterprise-T5120>root_server_hostname"=>"itgsyddev807.macbank",
+        "<SPARC-Enterprise-T5120>install_server_name"=>"someserver.somedomain",
+        "<SPARC-Enterprise-T5120>root_server_hostname"=>"someserver.somedomain",
         "<SPARC-Enterprise-T5120>root_server_ip"=>"192.168.122.24",
         "<SPARC-Enterprise-T5120>install_path"=>"/Solaris/install/Solaris_5.10_sparc_hw0811"
     }
