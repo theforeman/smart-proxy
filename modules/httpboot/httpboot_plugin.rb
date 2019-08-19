91c55c9e01e615461b0f9643fb15008d1cd63b04
Lukas Zapletal <lzap+git@redhat.com>
2019-08-19 11:10:45 +0200

    Fixes #27654 - expose http(s) port setting

--- modules/httpboot/httpboot_plugin.rb
+++ modules/httpboot/httpboot_plugin.rb
@@ -4,7 +4,11 @@ module Proxy::Httpboot
     https_rackup_path File.expand_path("http_config.ru", File.expand_path("../", __FILE__))
 
     plugin :httpboot, ::Proxy::VERSION
+    load_programmable_settings ::Proxy::Httpboot::PluginConfiguration
 
     default_settings :root_dir => '/var/lib/tftpboot'
+
+    expose_setting :http_port
+    expose_setting :https_port
   end
 end
