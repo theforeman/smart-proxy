f88b53d618b34b1f126f216d9eedfc7a46dcc244
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-08-23 15:26:24 +0200

    Refs #27654 - Move HTTP(S) enabled logic to plugin settings
    This introduces simple helpers to be used elsewhere. The Plugin Settings
    class makes more sense for this since it's specific to a plugin setting.

--- lib/proxy/settings/plugin.rb
+++ lib/proxy/settings/plugin.rb
@@ -1,6 +1,8 @@
 module ::Proxy::Settings
   class Plugin < ::OpenStruct
     SHARED_DEFAULTS = { :enabled => false }
+    HTTP_ENABLED = [true, 'http']
+    HTTPS_ENABLED = [true, 'https']
 
     attr_reader :defaults, :used_defaults
 
@@ -8,6 +10,14 @@ module ::Proxy::Settings
       @defaults = SHARED_DEFAULTS.merge(default_settings || {})
       @used_defaults = @defaults.keys - settings.keys
       super(@defaults.merge(settings))
+    end
+
+    def self.http_enabled?(setting)
+      HTTP_ENABLED.include?(setting)
+    end
+
+    def self.https_enabled?(setting)
+      HTTPS_ENABLED.include?(setting)
     end
   end
 end
