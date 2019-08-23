79ce65f72eae09baf5c0631c41422b813b390c8e
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-08-23 15:27:39 +0200

    Refs #27654 - Account for module enabled over http or not
    The module can be enabled for http-only while the proxy is https-only
    (or other combinations). This patch takes the enabled setting into
    account and guarantees that the http_port is only set when the module is
    actually available over http. The same goes for https_port.

--- modules/httpboot/httpboot_plugin_configuration.rb
+++
@@ -1,9 +0,0 @@
-module ::Proxy::Httpboot
-  class PluginConfiguration
-    def load_programmable_settings(settings)
-      settings[:http_port] = Proxy::SETTINGS.http_port
-      settings[:https_port] = Proxy::SETTINGS.https_port
-      settings
-    end
-  end
-end
--- test/httpboot/integration_test.rb
+++ test/httpboot/integration_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 require 'json'
 require 'root/root_v2_api'
-require 'httpboot/httpboot_plugin_configuration'
 require 'httpboot/httpboot_plugin'
 
 class HttpbootApiFeaturesTest < Test::Unit::TestCase
@@ -25,6 +24,26 @@ class HttpbootApiFeaturesTest < Test::Unit::TestCase
     assert_equal([], mod['capabilities'])
 
     expected_settings = {'http_port' => nil, 'https_port' => 8443}
+    assert_equal(expected_settings, mod['settings'])
+  end
+
+  def test_features_http
+    Proxy::SETTINGS.http_port = 1234
+    Proxy::SETTINGS.https_port = 5678
+    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('httpboot.yml').returns(enabled: 'http', root_dir: '/var/lib/tftpboot')
+
+    get '/features'
+
+    response = JSON.parse(last_response.body)
+
+    mod = response['httpboot']
+    refute_nil(mod)
+    assert(mod['http_enabled'])
+    refute(mod['https_enabled'])
+    assert_equal('running', mod['state'], Proxy::LogBuffer::Buffer.instance.info[:failed_modules][:httpboot])
+    assert_equal([], mod['capabilities'])
+
+    expected_settings = {'http_port' => 1234, 'https_port' => nil}
     assert_equal(expected_settings, mod['settings'])
   end
 end
