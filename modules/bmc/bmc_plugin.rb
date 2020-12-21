758f197d4b452424d59149abb11429fd6ab50465
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-12-21 15:07:47 +0100

    Fixes #31537 - Expose BMC providers as capabilities
    Currently the BMC module exposes an API endpoint /providers/installed
    but this can also be provided as capabilities or settings. This allows
    Foreman to determine up front if a proxy is even a valid choice without
    making API requests over the network.
    
    The load_classes part is needed because capabilities are exectuted
    before loading the main API (which already has the same require). The
    require is explicit in the tests to stub out the implementation, making
    the tests reliable, regardless of which providers are installed.

--- modules/bmc/bmc_plugin.rb
+++ modules/bmc/bmc_plugin.rb
@@ -5,5 +5,16 @@ module Proxy::BMC
     default_settings :redfish_verify_ssl => true
     validate :redfish_verify_ssl, :boolean => true
     plugin :bmc, ::Proxy::VERSION
+
+    # Various installed providers are exposed as capabilties
+    capability 'redfish'
+    capability 'shell'
+    capability 'ssh'
+    capability -> { Proxy::BMC::IPMI.providers_installed }
+
+    # Load IPMI to ensure the capabilities can be determined
+    load_classes do
+      require 'bmc/ipmi'
+    end
   end
 end
