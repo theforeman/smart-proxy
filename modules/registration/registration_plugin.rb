cf7973902afe42366e149705c5fc217b25f7d965
Leos Stejskal <lstejska@redhat.com>
2021-10-20 10:20:11 +0200

    Fixes #33756 - require 'templates' for 'registration' module

--- modules/registration/registration_plugin.rb
+++ modules/registration/registration_plugin.rb
@@ -4,5 +4,6 @@ module Proxy::Registration
     https_rackup_path File.expand_path("http_config.ru", File.expand_path(__dir__))
 
     plugin :registration, ::Proxy::VERSION
+    requires :templates, ::Proxy::VERSION
   end
 end
