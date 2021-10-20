6e6adfc1153e720a61d8b06582d4777dbece2874
Leos Stejskal <lstejska@redhat.com>
2021-10-20 10:20:11 +0200

    Fixes #33756 - require 'templates' for 'registration' module

--- test/registration/integration_test.rb
+++ test/registration/integration_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 require 'json'
 require 'root/root_v2_api'
+require 'templates/templates'
 require 'registration/registration'
 
 class RegistrationApiFeaturesTest < Test::Unit::TestCase
@@ -11,7 +12,8 @@ class RegistrationApiFeaturesTest < Test::Unit::TestCase
     Proxy::RootV2Api.new
   end
 
-  def test_features
+  def test_with_templates
+    Proxy::LegacyModuleLoader.any_instance.expects(:load_configuration_file).with('templates.yml').returns(enabled: true, template_url: 'http://smart-proxy.example.com:8000')
     Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('registration.yml').returns(enabled: true)
 
     get '/features'
@@ -20,5 +22,16 @@ class RegistrationApiFeaturesTest < Test::Unit::TestCase
 
     refute_nil(mod)
     assert_equal('running', mod['state'], Proxy::LogBuffer::Buffer.instance.info[:failed_modules][:registration])
+  end
+
+  def test_without_templates
+    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('registration.yml').returns(enabled: true)
+
+    get '/features'
+    response = JSON.parse(last_response.body)
+    mod = response['registration']
+
+    refute_nil(mod)
+    assert_equal('failed', mod['state'], Proxy::LogBuffer::Buffer.instance.info[:failed_modules][:registration])
   end
 end
