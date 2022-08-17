7586a7699d43612c2733470cb70d821db6c2fb97
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-08-17 18:26:12 +0200

    Refs #33881 - Remove unused Proxy::ProviderFactory
    Unused since 1.11.0 (42b958156808eb2a16bd895863b196fca8f2977d).

--- lib/proxy/provider.rb
+++ lib/proxy/provider.rb
@@ -2,17 +2,10 @@ class ::Proxy::Provider
   extend ::Proxy::Pluggable
 
   class << self
-    attr_reader :provider_factory
-
     def plugin(plugin_name, aversion, attrs = {})
       @plugin_name = plugin_name.to_sym
       @version = aversion.chomp('-develop')
-      @provider_factory = attrs[:factory]
       ::Proxy::Plugins.instance.plugin_loaded(@plugin_name, @version, self)
     end
-  end
-
-  def provider_factory
-    self.class.provider_factory
   end
 end
--- lib/proxy/provider_factory.rb
+++
@@ -1,9 +0,0 @@
-class ::Proxy::ProviderFactory
-  class << self
-    def get_provider(provider_name, opts = {})
-      provider = ::Proxy::Plugins.instance.find_provider(provider_name.to_sym)
-      pf = provider.provider_factory
-      pf.is_a?(Proc) ? pf.call(opts) : pf.new.get_provider(opts)
-    end
-  end
-end
