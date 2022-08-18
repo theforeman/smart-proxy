theforeman/smart-proxy

commit aa25357d4404908d5e5707e767ecd798b754d7d1

Fixes #35183 - Correctly call Timeout.timeout()
At least in Ruby 3.1 calling timeout() as a global method is no longer
allowed and Timetout.timeout is the correct way of calling it.

modules/dns_dnscmd/dns_dnscmd_main.rb
@@ -37,7 +37,7 @@ module Proxy::Dns::Dnscmd
       std_in = std_out = std_err = nil
       real_cmd = ['c:\Windows\System32\dnscmd.exe', @server] + args
       begin
-        timeout(tsecs) do
+        Timeout.timeout(tsecs) do
           logger.debug { "executing: #{real_cmd}" }
           std_in, std_out, std_err = popen3(*real_cmd)
           response = [std_out&.readlines, std_err&.readlines].flatten.compact

commit 22282fc7483f7a2439cc69c57d361860ea221fc9

Fixes #35384 - Add missing require 'timeout'
This explicitly requires timeout rather than depending on it implicitly.

Fixes: aa25357d4404908d5e5707e767ecd798b754d7d1

modules/dns_dnscmd/dns_dnscmd_main.rb
@@ -1,4 +1,5 @@
 require 'open3'
+require 'timeout'
 
 module Proxy::Dns::Dnscmd
   class Record < ::Proxy::Dns::Record