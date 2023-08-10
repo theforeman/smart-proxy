1996381a6fff6ba36b5f1d204edf6e83fdb9f96e
archanaserver <ak31960466@gmail.com>
2023-08-10 08:51:34 +0530

    Update RuboCop version to 1.56.0
    Bumped the RuboCop version from 1.28.0 to 1.56.0 to ensure the
    project benefits from the latest improvements, bug fixes, and
    code analysis features provided by the updated RuboCop release.

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -1,17 +1,17 @@
 group :test do
-  gem 'mocha', '~> 1.10', :require => false
-  gem 'ci_reporter', '>= 1.6.3', "< 2.0.0", :require => false
-  gem 'test-unit'
   gem 'benchmark-ips'
-  gem 'ruby-prof', '< 1.4'
-  gem 'rubocop', '~> 1.28.0'
-  gem 'rubocop-performance', '~> 1.5.2'
-  gem 'rack-test'
-  gem 'rake'
-  gem 'rubocop-checkstyle_formatter', '~> 0.2'
-  gem 'webmock'
+  gem 'ci_reporter', '>= 1.6.3', "< 2.0.0", :require => false
 
   # Technically this is a hard dependency of the facts module but that's only
   # used in discovery. This at least allows us to run the tests on it
   gem 'facter', :require => false
+  gem 'mocha', '~> 1.10', :require => false
+  gem 'rack-test'
+  gem 'rake'
+  gem 'rubocop', '~> 1.56.0'
+  gem 'rubocop-performance', '~> 1.5.2'
+  gem 'rubocop-rake'
+  gem 'ruby-prof', '< 1.4'
+  gem 'test-unit'
+  gem 'webmock'
 end
