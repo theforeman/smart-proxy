fc0646f61618a87c2d8dcd0ba7cfe15a9ec67f1a
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-01-05 14:56:50 +0100

    Use RUBY_PLATFORM to decide between rb-inotify and rb-kqueue
    This matches what modules/dhcp_isc/configuration_loader.rb does in
    load_programmable_settings.
    
    It then also merges into a single dhcp_isc bundler group. The rsec gem
    is limited to the ruby platfrom because you can't run dhcp_isc on
    Windows anyway.

--- bundler.d/dhcp_isc.rb
+++ bundler.d/dhcp_isc.rb
@@ -1,9 +1,5 @@
-gem 'rsec', '< 1', groups: [:dhcp_isc_inotify, :dhcp_isc_kqueue]
-
-group :dhcp_isc_inotify do
-  gem 'rb-inotify'
-end
-
-group :dhcp_isc_kqueue do
-  gem 'rb-kqueue'
+group :dhcp_isc do
+  gem 'rb-inotify', install_if: -> { RUBY_PLATFORM.match?(/linux/) }
+  gem 'rb-kqueue', install_if: -> { RUBY_PLATFORM.match?(/bsd/) }
+  gem 'rsec', '< 1', platforms: [:ruby]
 end
